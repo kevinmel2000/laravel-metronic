fb6157769494c433d31eb8c8bff0b228d88c0bf4
R. Kukuh <rkukuh@gmail.com>
2017-04-30 00:11:37 +0700

    Remove unused import

--- app/Models/Entrust/Permission.php
+++ app/Models/Entrust/Permission.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\Entrust;
 
-use Illuminate\Database\Eloquent\Model;
 use Zizaco\Entrust\EntrustPermission;
 
 class Permission extends EntrustPermission
--- app/Models/Entrust/Role.php
+++ app/Models/Entrust/Role.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\Entrust;
 
-use Illuminate\Database\Eloquent\Model;
 use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
