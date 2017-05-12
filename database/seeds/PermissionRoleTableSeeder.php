595c4f41a7102b6b4472f262e416ed1346f9e407
R. Kukuh <rkukuh@gmail.com>
2017-05-13 01:32:25 +0700

    Create seeder for Permission and Role relation

---
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Entrust\Role;
+use Illuminate\Database\Seeder;
+use App\Models\Entrust\Permission;
+
+class PermissionRoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = Role::where('name', 'admin')->first();
+        $permission_admin = Permission::where('name', 'admin-permission')->first();
+
+        $role_admin->attachPermission($permission_admin);
+
+        $role_user = Role::where('name', 'user')->first();
+        $permission_user  = Permission::where('name', 'user-permission')->first();
+
+        $role_user->attachPermission($permission_user);
+    }
+}
