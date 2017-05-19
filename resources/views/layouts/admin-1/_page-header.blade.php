b4305ea20e4b01329d1de59bd4c3ebb8a8dfa0a3
R. Kukuh <rkukuh@gmail.com>
2017-05-20 00:35:12 +0700

    Replace '/>' with '>' for single html element

--- resources/views/layouts/admin-1/_page-header.blade.php
+++ resources/views/layouts/admin-1/_page-header.blade.php
@@ -4,7 +4,7 @@
         <!-- BEGIN LOGO -->
         <div class="page-logo">
             <a href="index.html">
-                <img src="{{ asset('assets/layouts/layout/img/logo.png') }}" alt="logo" class="logo-default" /> </a>
+                <img src="{{ asset('assets/layouts/layout/img/logo.png') }}" alt="logo" class="logo-default"> </a>
             <div class="menu-toggler sidebar-toggler">
                 <span></span>
             </div>
@@ -311,7 +311,7 @@
                 <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                 <li class="dropdown dropdown-user">
                     <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                        <img alt="" class="img-circle" src="{{ asset('assets/layouts/layout/img/avatar3_small.jpg') }}" />
+                        <img alt="" class="img-circle" src="{{ asset('assets/layouts/layout/img/avatar3_small.jpg') }}">
                         <span class="username username-hide-on-mobile"> Nick </span>
                         <i class="fa fa-angle-down"></i>
                     </a>
