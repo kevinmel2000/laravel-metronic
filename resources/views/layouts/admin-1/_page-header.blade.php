0a7403ba8877d39d1155fa59701779436bb2f757
R. Kukuh <rkukuh@gmail.com>
2017-05-20 00:37:22 +0700

    Spaces

--- resources/views/layouts/admin-1/_page-header.blade.php
+++ resources/views/layouts/admin-1/_page-header.blade.php
@@ -1,6 +1,8 @@
 <div class="page-header navbar navbar-fixed-top">
+    
     <!-- BEGIN HEADER INNER -->
     <div class="page-header-inner ">
+
         <!-- BEGIN LOGO -->
         <div class="page-logo">
             <a href="index.html">
@@ -10,14 +12,17 @@
             </div>
         </div>
         <!-- END LOGO -->
+
         <!-- BEGIN RESPONSIVE MENU TOGGLER -->
         <a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
             <span></span>
         </a>
         <!-- END RESPONSIVE MENU TOGGLER -->
+
         <!-- BEGIN TOP NAVIGATION MENU -->
         <div class="top-menu">
             <ul class="nav navbar-nav pull-right">
+
                 <!-- BEGIN NOTIFICATION DROPDOWN -->
                 <!-- DOC: Apply "dropdown-dark" class after "dropdown-extended" to change the dropdown styte -->
                 <!-- DOC: Apply "dropdown-hoverable" class after below "dropdown" and remove data-toggle="dropdown" data-hover="dropdown" data-close-others="true" attributes to enable hover dropdown mode -->
@@ -121,6 +126,7 @@
                     </ul>
                 </li>
                 <!-- END NOTIFICATION DROPDOWN -->
+
                 <!-- BEGIN INBOX DROPDOWN -->
                 <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                 <li class="dropdown dropdown-extended dropdown-inbox" id="header_inbox_bar">
@@ -196,6 +202,7 @@
                     </ul>
                 </li>
                 <!-- END INBOX DROPDOWN -->
+
                 <!-- BEGIN TODO DROPDOWN -->
                 <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                 <li class="dropdown dropdown-extended dropdown-tasks" id="header_task_bar">
@@ -307,6 +314,7 @@
                     </ul>
                 </li>
                 <!-- END TODO DROPDOWN -->
+
                 <!-- BEGIN USER LOGIN DROPDOWN -->
                 <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                 <li class="dropdown dropdown-user">
@@ -348,6 +356,7 @@
                     </ul>
                 </li>
                 <!-- END USER LOGIN DROPDOWN -->
+
                 <!-- BEGIN QUICK SIDEBAR TOGGLER -->
                 <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                 <li class="dropdown dropdown-quick-sidebar-toggler">
@@ -356,9 +365,12 @@
                     </a>
                 </li>
                 <!-- END QUICK SIDEBAR TOGGLER -->
+
             </ul>
         </div>
         <!-- END TOP NAVIGATION MENU -->
+
     </div>
     <!-- END HEADER INNER -->
+
 </div>
