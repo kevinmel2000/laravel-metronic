e9ef8d27c87504d674110382dc4e3dd00b128133
R. Kukuh <rkukuh@gmail.com>
2017-05-20 00:17:11 +0700

    Syntax styling

--- resources/views/layouts/admin-1/_footer.blade.php
+++ resources/views/layouts/admin-1/_footer.blade.php
@@ -1,27 +1,27 @@
 <!--[if lt IE 9]>
-<script src="../assets/global/plugins/respond.min.js"></script>
-<script src="../assets/global/plugins/excanvas.min.js"></script>
-<script src="../assets/global/plugins/ie8.fix.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/respond.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/excanvas.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/ie8.fix.min.js"></script>
 <![endif]-->
 
 <!-- BEGIN CORE PLUGINS -->
-<script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="../assets/global/plugins/jquery.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/js.cookie.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/jquery.blockui.min.js"></script>
+<script type="text/javascript" src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
 <!-- END CORE PLUGINS -->
 
 <!-- BEGIN THEME GLOBAL SCRIPTS -->
-<script src="../assets/global/scripts/app.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="../assets/global/scripts/app.min.js"></script>
 <!-- END THEME GLOBAL SCRIPTS -->
 
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
-<script src="../assets/layouts/layout/scripts/layout.min.js" type="text/javascript"></script>
-<script src="../assets/layouts/layout/scripts/demo.min.js" type="text/javascript"></script>
-<script src="../assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
-<script src="../assets/layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="../assets/layouts/layout/scripts/layout.min.js"></script>
+<script type="text/javascript" src="../assets/layouts/layout/scripts/demo.min.js"></script>
+<script type="text/javascript" src="../assets/layouts/global/scripts/quick-sidebar.min.js"></script>
+<script type="text/javascript" src="../assets/layouts/global/scripts/quick-nav.min.js"></script>
 <!-- END THEME LAYOUT SCRIPTS -->
 
 <script>
