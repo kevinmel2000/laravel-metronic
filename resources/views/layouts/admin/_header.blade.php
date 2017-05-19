98b1c113245b8426faecfa0e3bbbed4b5e0149a5
R. Kukuh <rkukuh@gmail.com>
2017-05-19 22:54:09 +0700

    Syntax styling

--- resources/views/layouts/admin/_header.blade.php
+++ resources/views/layouts/admin/_header.blade.php
@@ -10,22 +10,22 @@
         <meta content="" name="author">
 
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css">
         <!-- END GLOBAL MANDATORY STYLES -->
 
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css">
-        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/css/components.min.css" id="style_components">
+        <link rel="stylesheet" type="text/css" href="../assets/global/css/plugins.min.css">
         <!-- END THEME GLOBAL STYLES -->
 
         <!-- BEGIN THEME LAYOUT STYLES -->
-        <link href="../assets/layouts/layout/css/layout.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/layouts/layout/css/themes/darkblue.min.css" rel="stylesheet" type="text/css" id="style_color">
-        <link href="../assets/layouts/layout/css/custom.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../assets/layouts/layout/css/layout.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/layouts/layout/css/themes/darkblue.min.css" id="style_color">
+        <link rel="stylesheet" type="text/css" href="../assets/layouts/layout/css/custom.min.css">
         <!-- END THEME LAYOUT STYLES -->
 
         <link rel="shortcut icon" href="favicon.ico">
