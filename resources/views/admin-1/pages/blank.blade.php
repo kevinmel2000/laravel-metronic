c17aaa9b5f24ee447da589dbe492ca8ab226a547
R. Kukuh <rkukuh@gmail.com>
2017-05-20 00:55:35 +0700

    Fill breadcrumb section

--- resources/views/admin-1/pages/blank.blade.php
+++ resources/views/admin-1/pages/blank.blade.php
@@ -1,1 +1,17 @@
 @extends('layouts.admin-1.master')
+
+@section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <a href="#">Blank Page</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Page Layouts</span>
+        </li>
+    </ul>
+@endsection
