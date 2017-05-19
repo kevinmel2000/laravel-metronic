c5ceb3e1c8058b1eba834c9a8e0dbe0b87c64030
R. Kukuh <rkukuh@gmail.com>
2017-05-20 00:20:24 +0700

    Use asset() instead

--- resources/views/layouts/admin-1/_footer.blade.php
+++ resources/views/layouts/admin-1/_footer.blade.php
@@ -1,27 +1,27 @@
 <!--[if lt IE 9]>
-<script type="text/javascript" src="../assets/global/plugins/respond.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/excanvas.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/ie8.fix.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/respond.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/excanvas.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/ie8.fix.min.js') }}"></script>
 <![endif]-->
 
 <!-- BEGIN CORE PLUGINS -->
-<script type="text/javascript" src="../assets/global/plugins/jquery.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/bootstrap/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/js.cookie.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/jquery.blockui.min.js"></script>
-<script type="text/javascript" src="../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/js.cookie.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.blockui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
 <!-- END CORE PLUGINS -->
 
 <!-- BEGIN THEME GLOBAL SCRIPTS -->
-<script type="text/javascript" src="../assets/global/scripts/app.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/global/scripts/app.min.js') }}"></script>
 <!-- END THEME GLOBAL SCRIPTS -->
 
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
-<script type="text/javascript" src="../assets/layouts/layout/scripts/layout.min.js"></script>
-<script type="text/javascript" src="../assets/layouts/layout/scripts/demo.min.js"></script>
-<script type="text/javascript" src="../assets/layouts/global/scripts/quick-sidebar.min.js"></script>
-<script type="text/javascript" src="../assets/layouts/global/scripts/quick-nav.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/layouts/layout/scripts/layout.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/layouts/layout/scripts/demo.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/layouts/global/scripts/quick-sidebar.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/layouts/global/scripts/quick-nav.min.js') }}"></script>
 <!-- END THEME LAYOUT SCRIPTS -->
 
 <script>
