ec6d680f1197410fe1d01ecfc0904ed11d9bcf18
R. Kukuh <rkukuh@gmail.com>
2017-05-19 23:05:22 +0700

    Use asset() instead

--- resources/views/layouts/admin/_header.blade.php
+++ resources/views/layouts/admin/_header.blade.php
@@ -12,21 +12,21 @@
 
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all">
-        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css">
-        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/font-awesome/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}">
         <!-- END GLOBAL MANDATORY STYLES -->
 
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link rel="stylesheet" type="text/css" href="../assets/global/css/components.min.css" id="style_components">
-        <link rel="stylesheet" type="text/css" href="../assets/global/css/plugins.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/css/components.min.css') }}" id="style_components">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/css/plugins.min.css') }}">
         <!-- END THEME GLOBAL STYLES -->
 
         <!-- BEGIN THEME LAYOUT STYLES -->
-        <link rel="stylesheet" type="text/css" href="../assets/layouts/layout/css/layout.min.css">
-        <link rel="stylesheet" type="text/css" href="../assets/layouts/layout/css/themes/darkblue.min.css" id="style_color">
-        <link rel="stylesheet" type="text/css" href="../assets/layouts/layout/css/custom.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/layout.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/themes/darkblue.min.css') }}" id="style_color">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/custom.min.css') }}">
         <!-- END THEME LAYOUT STYLES -->
 
         <link rel="shortcut icon" href="favicon.ico">
