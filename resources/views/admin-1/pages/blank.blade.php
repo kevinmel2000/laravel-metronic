c828b2d410d40576db906a2895f1b6f9be61149d
R. Kukuh <rkukuh@gmail.com>
2017-05-20 01:08:08 +0700

    Fill content section

--- resources/views/admin-1/pages/blank.blade.php
+++ resources/views/admin-1/pages/blank.blade.php
@@ -15,3 +15,13 @@
         </li>
     </ul>
 @endsection
+
+@section('content')
+    <h1 class="page-title"> Blank Page Layout
+        <small>blank page layout</small>
+    </h1>
+
+    <div class="note note-info">
+        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+    </div>
+@endsection
