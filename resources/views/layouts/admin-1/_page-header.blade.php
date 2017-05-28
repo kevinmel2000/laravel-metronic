ffd7a2152f5151c57e7dcfddd1a92980e1ed767e
R. Kukuh <rkukuh@gmail.com>
2017-05-29 01:38:57 +0700

    Remove unused comments

--- resources/views/layouts/admin-1/_page-header.blade.php
+++ resources/views/layouts/admin-1/_page-header.blade.php
@@ -1,25 +1,19 @@
 <div class="page-header navbar navbar-fixed-top">
-
-    <!-- BEGIN HEADER INNER -->
     <div class="page-header-inner ">
 
-        <!-- BEGIN LOGO -->
         <div class="page-logo">
             <a href="index.html">
-                <img src="{{ asset('assets/layouts/layout/img/logo.png') }}" alt="logo" class="logo-default"> </a>
+                <img src="{{ asset('assets/layouts/layout/img/logo.png') }}" alt="logo" class="logo-default">
+            </a>
             <div class="menu-toggler sidebar-toggler">
                 <span></span>
             </div>
         </div>
-        <!-- END LOGO -->
 
-        <!-- BEGIN RESPONSIVE MENU TOGGLER -->
         <a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
             <span></span>
         </a>
-        <!-- END RESPONSIVE MENU TOGGLER -->
 
-        <!-- BEGIN TOP NAVIGATION MENU -->
         <div class="top-menu">
             <ul class="nav navbar-nav pull-right">
 
@@ -35,9 +29,6 @@
 
             </ul>
         </div>
-        <!-- END TOP NAVIGATION MENU -->
 
     </div>
-    <!-- END HEADER INNER -->
-
 </div>
