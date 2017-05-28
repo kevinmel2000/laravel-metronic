d40ed62d15959fb1c127c046ad360c3738633792
R. Kukuh <rkukuh@gmail.com>
2017-05-29 01:37:12 +0700

    Include quick sidebar partial view

--- resources/views/layouts/admin-1/_page-header.blade.php
+++ resources/views/layouts/admin-1/_page-header.blade.php
@@ -31,14 +31,7 @@
 
                 @include('layouts.admin-1.navbar.user')
 
-                <!-- BEGIN QUICK SIDEBAR TOGGLER -->
-                <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
-                <li class="dropdown dropdown-quick-sidebar-toggler">
-                    <a href="javascript:;" class="dropdown-toggle">
-                        <i class="icon-logout"></i>
-                    </a>
-                </li>
-                <!-- END QUICK SIDEBAR TOGGLER -->
+                @include('layouts.admin-1.navbar.quick-sidebar')
 
             </ul>
         </div>
