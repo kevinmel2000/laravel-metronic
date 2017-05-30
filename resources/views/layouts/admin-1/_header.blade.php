837f0beaa3cb5cc2f26b277396a8d036720ac4c3
R. Kukuh <rkukuh@gmail.com>
2017-05-31 01:45:27 +0700

    Update content

--- resources/views/layouts/admin-1/_header.blade.php
+++ resources/views/layouts/admin-1/_header.blade.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }} | Admin #1 | Blank Page Layout</title>
+<title>{{ config('app.name', 'Laravel') }} | Admin #1 | Blank Page Layout</title>
 
-        <meta name="description" content="Preview of {{ config('app.name', 'Laravel') }} Admin #1 Blank Page layout">
-        <meta name="author" content="R. Kukuh">
+<meta name="description" content="Preview of {{ config('app.name', 'Laravel') }} Admin #1 Blank Page layout">
+<meta name="author" content="R. Kukuh">
 
-        <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/font-awesome/css/font-awesome.min.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}">
-        <!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/font-awesome/css/font-awesome.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}">
+<!-- END GLOBAL MANDATORY STYLES -->
 
-        <!-- BEGIN THEME GLOBAL STYLES -->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/css/components.min.css') }}" id="style_components">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/css/plugins.min.css') }}">
-        <!-- END THEME GLOBAL STYLES -->
+<!-- BEGIN THEME GLOBAL STYLES -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/global/css/components.min.css') }}" id="style_components">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/global/css/plugins.min.css') }}">
+<!-- END THEME GLOBAL STYLES -->
 
-        <!-- BEGIN THEME LAYOUT STYLES -->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/layout.min.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/themes/darkblue.min.css') }}" id="style_color">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/custom.min.css') }}">
-        <!-- END THEME LAYOUT STYLES -->
+<!-- BEGIN THEME LAYOUT STYLES -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/layout.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/themes/darkblue.min.css') }}" id="style_color">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/layouts/layout/css/custom.min.css') }}">
+<!-- END THEME LAYOUT STYLES -->
 
-        <link rel="shortcut icon" href="favicon.ico">
-    </head>
+<link rel="shortcut icon" href="favicon.ico">
