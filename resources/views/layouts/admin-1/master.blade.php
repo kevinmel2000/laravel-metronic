e258894fd6f6a4e40b300eaee79166d926220090
R. Kukuh <rkukuh@gmail.com>
2017-05-31 01:45:21 +0700

    Update content

--- resources/views/layouts/admin-1/master.blade.php
+++ resources/views/layouts/admin-1/master.blade.php
@@ -1,5 +1,13 @@
-@include('layouts.admin-1._header')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        @include('layouts.admin-1._header')
+        
+    </head>
     <body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white">
         <div class="page-wrapper">
 
