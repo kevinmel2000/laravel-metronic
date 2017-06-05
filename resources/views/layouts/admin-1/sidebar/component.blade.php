4251fb64bc0d75af02e00b6a116c53f83b416ced
R. Kukuh <rkukuh@gmail.com>
2017-06-06 02:04:15 +0700

    Create partial view for sidebar's component menu

---
+++ resources/views/layouts/admin-1/sidebar/component.blade.php
@@ -0,0 +1,116 @@
+<li class="nav-item  ">
+    <a href="javascript:;" class="nav-link nav-toggle">
+        <i class="icon-puzzle"></i>
+        <span class="title">Components</span>
+        <span class="arrow"></span>
+    </a>
+    <ul class="sub-menu">
+        <li class="nav-item  ">
+            <a href="components_date_time_pickers.html" class="nav-link ">
+                <span class="title">Date & Time Pickers</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_color_pickers.html" class="nav-link ">
+                <span class="title">Color Pickers</span>
+                <span class="badge badge-danger">2</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_select2.html" class="nav-link ">
+                <span class="title">Select2 Dropdowns</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_select.html" class="nav-link ">
+                <span class="title">Bootstrap Select</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_multi_select.html" class="nav-link ">
+                <span class="title">Bootstrap Multiple Select</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_multiselect_dropdown.html" class="nav-link ">
+                <span class="title">Bootstrap Multiselect Dropdowns</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_select_splitter.html" class="nav-link ">
+                <span class="title">Select Splitter</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_clipboard.html" class="nav-link ">
+                <span class="title">Clipboard</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_typeahead.html" class="nav-link ">
+                <span class="title">Typeahead Autocomplete</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_tagsinput.html" class="nav-link ">
+                <span class="title">Bootstrap Tagsinput</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_switch.html" class="nav-link ">
+                <span class="title">Bootstrap Switch</span>
+                <span class="badge badge-success">6</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_maxlength.html" class="nav-link ">
+                <span class="title">Bootstrap Maxlength</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_fileinput.html" class="nav-link ">
+                <span class="title">Bootstrap File Input</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_bootstrap_touchspin.html" class="nav-link ">
+                <span class="title">Bootstrap Touchspin</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_form_tools.html" class="nav-link ">
+                <span class="title">Form Widgets & Tools</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_context_menu.html" class="nav-link ">
+                <span class="title">Context Menu</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_editors.html" class="nav-link ">
+                <span class="title">Markdown & WYSIWYG Editors</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_code_editors.html" class="nav-link ">
+                <span class="title">Code Editors</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_ion_sliders.html" class="nav-link ">
+                <span class="title">Ion Range Sliders</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_noui_sliders.html" class="nav-link ">
+                <span class="title">NoUI Range Sliders</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="components_knob_dials.html" class="nav-link ">
+                <span class="title">Knob Circle Dials</span>
+            </a>
+        </li>
+    </ul>
+</li>
