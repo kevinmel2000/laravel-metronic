dc6d3a97be91e039c43f4c59d17039c7ecf03900
R. Kukuh <rkukuh@gmail.com>
2017-06-06 01:58:38 +0700

    Create partial view for sidebar's dashboard menu

---
+++ resources/views/layouts/admin-1/sidebar/dashboard.blade.php
@@ -0,0 +1,29 @@
+<li class="nav-item start ">
+    <a href="javascript:;" class="nav-link nav-toggle">
+        <i class="icon-home"></i>
+        <span class="title">Dashboard</span>
+        <span class="arrow"></span>
+    </a>
+    <ul class="sub-menu">
+        <li class="nav-item start ">
+            <a href="index.html" class="nav-link ">
+                <i class="icon-bar-chart"></i>
+                <span class="title">Dashboard 1</span>
+            </a>
+        </li>
+        <li class="nav-item start ">
+            <a href="dashboard_2.html" class="nav-link ">
+                <i class="icon-bulb"></i>
+                <span class="title">Dashboard 2</span>
+                <span class="badge badge-success">1</span>
+            </a>
+        </li>
+        <li class="nav-item start ">
+            <a href="dashboard_3.html" class="nav-link ">
+                <i class="icon-graph"></i>
+                <span class="title">Dashboard 3</span>
+                <span class="badge badge-danger">5</span>
+            </a>
+        </li>
+    </ul>
+</li>
