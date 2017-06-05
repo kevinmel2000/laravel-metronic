eedfafa713569eb1ea009385cc5410b822457a09
R. Kukuh <rkukuh@gmail.com>
2017-06-06 02:05:56 +0700

    Remove unused comment

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -15,13 +15,11 @@
         <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200" style="padding-top: 20px">
 
             <!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
-            <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
             <li class="sidebar-toggler-wrapper hide">
                 <div class="sidebar-toggler">
                     <span></span>
                 </div>
             </li>
-            <!-- END SIDEBAR TOGGLER BUTTON -->
 
             @include('layouts.admin-1.sidebar.search-form')
 
