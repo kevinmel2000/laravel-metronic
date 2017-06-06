8977db25c1f8b7ac44c093dc8cad07ce0b15a12f
R. Kukuh <rkukuh@gmail.com>
2017-06-07 00:06:55 +0700

    Create partial view for sidebar's custom layout menu

---
+++ resources/views/layouts/admin-1/sidebar/custom-layout.blade.php
@@ -0,0 +1,24 @@
+<li class="nav-item  ">
+    <a href="javascript:;" class="nav-link nav-toggle">
+        <i class=" icon-wrench"></i>
+        <span class="title">Custom Layouts</span>
+        <span class="arrow"></span>
+    </a>
+    <ul class="sub-menu">
+        <li class="nav-item  ">
+            <a href="layout_disabled_menu.html" class="nav-link ">
+                <span class="title">Disabled Menu Links</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_full_height_portlet.html" class="nav-link ">
+                <span class="title">Full Height Portlet</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_full_height_content.html" class="nav-link ">
+                <span class="title">Full Height Content</span>
+            </a>
+        </li>
+    </ul>
+</li>
