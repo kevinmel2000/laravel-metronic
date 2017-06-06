d1c6152d4656469eb9786b672472226c025ec336
R. Kukuh <rkukuh@gmail.com>
2017-06-07 00:04:43 +0700

    Create partial view for sidebar's horizontal menu

---
+++ resources/views/layouts/admin-1/sidebar/horizontal-menu.blade.php
@@ -0,0 +1,24 @@
+<li class="nav-item  ">
+    <a href="javascript:;" class="nav-link nav-toggle">
+        <i class="icon-paper-plane"></i>
+        <span class="title">Horizontal Menu</span>
+        <span class="arrow"></span>
+    </a>
+    <ul class="sub-menu">
+        <li class="nav-item  ">
+            <a href="layout_mega_menu_light.html" class="nav-link ">
+                <span class="title">Light Mega Menu</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_mega_menu_dark.html" class="nav-link ">
+                <span class="title">Dark Mega Menu</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_full_width.html" class="nav-link ">
+                <span class="title">Full Width Layout</span>
+            </a>
+        </li>
+    </ul>
+</li>
