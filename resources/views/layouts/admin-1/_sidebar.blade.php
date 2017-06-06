4dd3340dd8e285988f109ef4a77188e6dee67fb9
R. Kukuh <rkukuh@gmail.com>
2017-06-07 00:07:10 +0700

    Include sidebar's custom layout menu

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -55,30 +55,7 @@
 
             @include('layouts.admin-1.sidebar.horizontal-menu')
 
-            <li class="nav-item  ">
-                <a href="javascript:;" class="nav-link nav-toggle">
-                    <i class=" icon-wrench"></i>
-                    <span class="title">Custom Layouts</span>
-                    <span class="arrow"></span>
-                </a>
-                <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="layout_disabled_menu.html" class="nav-link ">
-                            <span class="title">Disabled Menu Links</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_full_height_portlet.html" class="nav-link ">
-                            <span class="title">Full Height Portlet</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_full_height_content.html" class="nav-link ">
-                            <span class="title">Full Height Content</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
+            @include('layouts.admin-1.sidebar.custom-layout')
 
             <li class="heading">
                 <h3 class="uppercase">Pages</h3>
