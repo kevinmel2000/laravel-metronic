764ab22452ef151d8ed896e3a60b25dc741d648e
R. Kukuh <rkukuh@gmail.com>
2017-06-07 00:26:10 +0700

    Include sidebar's app menu

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -63,45 +63,7 @@
 
             @include('layouts.admin-1.sidebar.ecommerce')
 
-            <li class="nav-item  ">
-                <a href="javascript:;" class="nav-link nav-toggle">
-                    <i class="icon-docs"></i>
-                    <span class="title">Apps</span>
-                    <span class="arrow"></span>
-                </a>
-                <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="app_todo.html" class="nav-link ">
-                            <i class="icon-clock"></i>
-                            <span class="title">Todo 1</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="app_todo_2.html" class="nav-link ">
-                            <i class="icon-check"></i>
-                            <span class="title">Todo 2</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="app_inbox.html" class="nav-link ">
-                            <i class="icon-envelope"></i>
-                            <span class="title">Inbox</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="app_calendar.html" class="nav-link ">
-                            <i class="icon-calendar"></i>
-                            <span class="title">Calendar</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="app_ticket.html" class="nav-link ">
-                            <i class="icon-notebook"></i>
-                            <span class="title">Support</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
+            @include('layouts.admin-1.sidebar.app')
 
             <li class="nav-item  ">
                 <a href="javascript:;" class="nav-link nav-toggle">
