45b037d09b422f46a33f91e9d6d50482643e4e8f
R. Kukuh <rkukuh@gmail.com>
2017-06-07 00:05:01 +0700

    Include sidebar's horizontal menu

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -53,30 +53,7 @@
 
             @include('layouts.admin-1.sidebar.sidebar-layout')
 
-            <li class="nav-item  ">
-                <a href="javascript:;" class="nav-link nav-toggle">
-                    <i class="icon-paper-plane"></i>
-                    <span class="title">Horizontal Menu</span>
-                    <span class="arrow"></span>
-                </a>
-                <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="layout_mega_menu_light.html" class="nav-link ">
-                            <span class="title">Light Mega Menu</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_mega_menu_dark.html" class="nav-link ">
-                            <span class="title">Dark Mega Menu</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_full_width.html" class="nav-link ">
-                            <span class="title">Full Width Layout</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
+            @include('layouts.admin-1.sidebar.horizontal-menu')
 
             <li class="nav-item  ">
                 <a href="javascript:;" class="nav-link nav-toggle">
