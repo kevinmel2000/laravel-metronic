9a35ec5cfb1337eb19ae6e9772cfba40493c02db
R. Kukuh <rkukuh@gmail.com>
2017-06-07 00:02:50 +0700

    Include sidebar's sidebar layout menu

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -51,55 +51,7 @@
 
             @include('layouts.admin-1.sidebar.page-layout')
 
-            <li class="nav-item  ">
-                <a href="javascript:;" class="nav-link nav-toggle">
-                    <i class="icon-feed"></i>
-                    <span class="title">Sidebar Layouts</span>
-                    <span class="arrow"></span>
-                </a>
-                <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_menu_light.html" class="nav-link ">
-                            <span class="title">Light Sidebar Menu</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_menu_hover.html" class="nav-link ">
-                            <span class="title">Hover Sidebar Menu</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_search_1.html" class="nav-link ">
-                            <span class="title">Sidebar Search Option 1</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_search_2.html" class="nav-link ">
-                            <span class="title">Sidebar Search Option 2</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_toggler_on_sidebar.html" class="nav-link ">
-                            <span class="title">Sidebar Toggler On Sidebar</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_reversed.html" class="nav-link ">
-                            <span class="title">Reversed Sidebar Page</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_fixed.html" class="nav-link ">
-                            <span class="title">Fixed Sidebar Layout</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="layout_sidebar_closed.html" class="nav-link ">
-                            <span class="title">Closed Sidebar Layout</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
+            @include('layouts.admin-1.sidebar.sidebar-layout')
 
             <li class="nav-item  ">
                 <a href="javascript:;" class="nav-link nav-toggle">
