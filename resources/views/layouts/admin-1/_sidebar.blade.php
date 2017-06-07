a0f918df7b7ed90afc4ace271b750cd26a9bbd4f
R. Kukuh <rkukuh@gmail.com>
2017-06-07 23:48:09 +0700

    Include sidebar map menu

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -43,25 +43,7 @@
 
             @include('layouts.admin-1.sidebar.chart')
 
-            <li class="nav-item  ">
-                <a href="javascript:;" class="nav-link nav-toggle">
-                    <i class="icon-pointer"></i>
-                    <span class="title">Maps</span>
-                    <span class="arrow"></span>
-                </a>
-                <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="maps_google.html" class="nav-link ">
-                            <span class="title">Google Maps</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="maps_vector.html" class="nav-link ">
-                            <span class="title">Vector Maps</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
+            @include('layouts.admin-1.sidebar.map')
 
             <li class="heading">
                 <h3 class="uppercase">Layouts</h3>
