d6044f565005008900f5cd04aa22a62db9d70317
R. Kukuh <rkukuh@gmail.com>
2017-06-07 23:46:52 +0700

    Include sidebar chart menu

--- resources/views/layouts/admin-1/_sidebar.blade.php
+++ resources/views/layouts/admin-1/_sidebar.blade.php
@@ -41,62 +41,7 @@
 
             @include('layouts.admin-1.sidebar.portlet')
 
-            <li class="nav-item  ">
-                <a href="javascript:;" class="nav-link nav-toggle">
-                    <i class="icon-bar-chart"></i>
-                    <span class="title">Charts</span>
-                    <span class="arrow"></span>
-                </a>
-                <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="charts_amcharts.html" class="nav-link ">
-                            <span class="title">amChart</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="charts_flotcharts.html" class="nav-link ">
-                            <span class="title">Flot Charts</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="charts_flowchart.html" class="nav-link ">
-                            <span class="title">Flow Charts</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="charts_google.html" class="nav-link ">
-                            <span class="title">Google Charts</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="charts_echarts.html" class="nav-link ">
-                            <span class="title">eCharts</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="charts_morris.html" class="nav-link ">
-                            <span class="title">Morris Charts</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="javascript:;" class="nav-link nav-toggle">
-                            <span class="title">HighCharts</span>
-                            <span class="arrow"></span>
-                        </a>
-                        <ul class="sub-menu">
-                            <li class="nav-item ">
-                                <a href="charts_highcharts.html" class="nav-link "> HighCharts </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a href="charts_highstock.html" class="nav-link "> HighStock </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a href="charts_highmaps.html" class="nav-link "> HighMaps </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+            @include('layouts.admin-1.sidebar.chart')
 
             <li class="nav-item  ">
                 <a href="javascript:;" class="nav-link nav-toggle">
