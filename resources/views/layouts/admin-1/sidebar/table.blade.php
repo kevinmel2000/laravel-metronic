e846c51842c33029c2f9ebced6e83278fc7f23de
R. Kukuh <rkukuh@gmail.com>
2017-06-07 23:32:58 +0700

    Create partial view for sidebar table menu

---
+++ resources/views/layouts/admin-1/sidebar/table.blade.php
@@ -0,0 +1,59 @@
+<li class="nav-item  ">
+    <a href="javascript:;" class="nav-link nav-toggle">
+        <i class="icon-briefcase"></i>
+        <span class="title">Tables</span>
+        <span class="arrow"></span>
+    </a>
+    <ul class="sub-menu">
+        <li class="nav-item  ">
+            <a href="table_static_basic.html" class="nav-link ">
+                <span class="title">Basic Tables</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="table_static_responsive.html" class="nav-link ">
+                <span class="title">Responsive Tables</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="table_bootstrap.html" class="nav-link ">
+                <span class="title">Bootstrap Tables</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="javascript:;" class="nav-link nav-toggle">
+                <span class="title">Datatables</span>
+                <span class="arrow"></span>
+            </a>
+            <ul class="sub-menu">
+                <li class="nav-item ">
+                    <a href="table_datatables_managed.html" class="nav-link "> Managed Datatables </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_buttons.html" class="nav-link "> Buttons Extension </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_colreorder.html" class="nav-link "> Colreorder Extension </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_rowreorder.html" class="nav-link "> Rowreorder Extension </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_scroller.html" class="nav-link "> Scroller Extension </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_fixedheader.html" class="nav-link "> FixedHeader Extension </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_responsive.html" class="nav-link "> Responsive Extension </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_editable.html" class="nav-link "> Editable Datatables </a>
+                </li>
+                <li class="nav-item ">
+                    <a href="table_datatables_ajax.html" class="nav-link "> Ajax Datatables </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</li>
