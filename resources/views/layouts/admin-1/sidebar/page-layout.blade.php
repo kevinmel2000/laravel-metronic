2d469f46bfd9c5d78c76ecbaa412a32f9940ab62
R. Kukuh <rkukuh@gmail.com>
2017-06-07 23:55:01 +0700

    Create partial view for sidebar page layout menu

---
+++ resources/views/layouts/admin-1/sidebar/page-layout.blade.php
@@ -0,0 +1,56 @@
+<li class="nav-item  active open">
+    <a href="javascript:;" class="nav-link nav-toggle">
+        <i class="icon-layers"></i>
+        <span class="title">Page Layouts</span>
+        <span class="selected"></span>
+        <span class="arrow open"></span>
+    </a>
+    <ul class="sub-menu">
+        <li class="nav-item  active open">
+            <a href="layout_blank_page.html" class="nav-link ">
+                <span class="title">Blank Page</span>
+                <span class="selected"></span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_classic_page_head.html" class="nav-link ">
+                <span class="title">Classic Page Head</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_light_page_head.html" class="nav-link ">
+                <span class="title">Light Page Head</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_content_grey.html" class="nav-link ">
+                <span class="title">Grey Bg Content</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_search_on_header_1.html" class="nav-link ">
+                <span class="title">Search Box On Header 1</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_search_on_header_2.html" class="nav-link ">
+                <span class="title">Search Box On Header 2</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_language_bar.html" class="nav-link ">
+                <span class="title">Header Language Bar</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_footer_fixed.html" class="nav-link ">
+                <span class="title">Fixed Footer</span>
+            </a>
+        </li>
+        <li class="nav-item  ">
+            <a href="layout_boxed_page.html" class="nav-link ">
+                <span class="title">Boxed Page</span>
+            </a>
+        </li>
+    </ul>
+</li>
