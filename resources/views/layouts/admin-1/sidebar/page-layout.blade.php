38cd6c18051d8c4b4e5c2035f85d394af60436fb
R. Kukuh <rkukuh@gmail.com>
2017-06-13 01:14:07 +0700

    No open sidebar menu at all (at this moment)

--- resources/views/layouts/admin-1/sidebar/page-layout.blade.php
+++ resources/views/layouts/admin-1/sidebar/page-layout.blade.php
@@ -1,15 +1,13 @@
-<li class="nav-item  active open">
+<li class="nav-item  ">
     <a href="javascript:;" class="nav-link nav-toggle">
         <i class="icon-layers"></i>
         <span class="title">Page Layouts</span>
-        <span class="selected"></span>
-        <span class="arrow open"></span>
+        <span class="arrow"></span>
     </a>
     <ul class="sub-menu">
-        <li class="nav-item  active open">
+        <li class="nav-item  ">
             <a href="layout_blank_page.html" class="nav-link ">
                 <span class="title">Blank Page</span>
-                <span class="selected"></span>
             </a>
         </li>
         <li class="nav-item  ">
