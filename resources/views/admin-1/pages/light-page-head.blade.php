6ebd321d485fa051edd207323a48e141ac1f5fef
R. Kukuh <rkukuh@gmail.com>
2017-06-20 21:16:21 +0700

    Update breadcrumb and page content

--- resources/views/admin-1/pages/light-page-head.blade.php
+++ resources/views/admin-1/pages/light-page-head.blade.php
@@ -3,9 +3,23 @@
 @section('title', 'Light Page Head')
 
 @section('breadcrumb')
-    // breadcrumb
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Page Layouts</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
-    // content
+    <h1 class="page-title"> Light Page Head
+        <small>light page head option</small>
+    </h1>
+
+    <div class="note note-info">
+        <p> Light page head with page title, breadcrumbs and action dropdown menu. </p>
+    </div>
 @endsection
