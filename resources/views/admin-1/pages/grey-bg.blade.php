f70512e91a9e1ea4a1d78c33ccaf48093158c994
R. Kukuh <rkukuh@gmail.com>
2017-06-20 22:04:28 +0700

    Update breadcrumb and page content

--- resources/views/admin-1/pages/grey-bg.blade.php
+++ resources/views/admin-1/pages/grey-bg.blade.php
@@ -3,9 +3,25 @@
 @section('title', 'Grey Bg Content')
 
 @section('breadcrumb')
-    // breadcrumb
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-angle-right"></i>
+        </li>
+        <li>
+            <span>Page Layouts</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
-    // content
+    {{-- TODO: How do make this content grey colored? --}}
+    
+    <h1 class="page-title"> Grey Bg Content
+        <small>grey bg content sample</small>
+    </h1>
+
+    <div class="note note-info">
+        <p> Grey content page with page title, breadcrumbs and action dropdown menu. </p>
+    </div>
 @endsection
