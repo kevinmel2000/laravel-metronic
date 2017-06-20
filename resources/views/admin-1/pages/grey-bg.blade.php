7bb8237ed76677eebdb547944385d6dd66bbc205
R. Kukuh <rkukuh@gmail.com>
2017-06-20 21:59:54 +0700

    Create and initialize partial view

---
+++ resources/views/admin-1/pages/grey-bg.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin-1.master')
+
+@section('title', 'Grey Bg Content')
+
+@section('breadcrumb')
+    // breadcrumb
+@endsection
+
+@section('content')
+    // content
+@endsection
