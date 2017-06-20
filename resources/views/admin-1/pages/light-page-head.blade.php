ab7069025746e0bb78f1a127fd462e028c55b9be
R. Kukuh <rkukuh@gmail.com>
2017-06-20 21:02:23 +0700

    Create and initialize partial view

---
+++ resources/views/admin-1/pages/light-page-head.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin-1.master')
+
+@section('title', 'Light Page Head')
+
+@section('breadcrumb')
+    // breadcrumb
+@endsection
+
+@section('content')
+    // content
+@endsection
