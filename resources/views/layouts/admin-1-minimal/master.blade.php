561bed2bfd14599d0928ad0278383a5a3d07671d
R. Kukuh <rkukuh@gmail.com>
2017-06-21 21:00:15 +0700

    Fix: Included 'admin-1-minimal' on master layout

--- resources/views/layouts/admin-1-minimal/master.blade.php
+++ resources/views/layouts/admin-1-minimal/master.blade.php
@@ -5,45 +5,45 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @include('layouts.admin-1._header')
+        @include('layouts.admin-1-minimal._header')
 
         @stack('header-script')
     </head>
     <body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white">
         <div class="page-wrapper">
 
-            @include('layouts.admin-1._navbar')
+            @include('layouts.admin-1-minimal._navbar')
 
             <div class="clearfix"> </div>
 
             <div class="page-container">
 
-                @include('layouts.admin-1._sidebar')
+                @include('layouts.admin-1-minimal._sidebar')
 
                 <div class="page-content-wrapper">
                     <div class="page-content">
 
-                        @include('layouts.admin-1._theme-panel')
+                        @include('layouts.admin-1-minimal._theme-panel')
 
                         <div class="page-bar">
                             @yield('breadcrumb')
 
-                            @include('layouts.admin-1._page-toolbar')
+                            @include('layouts.admin-1-minimal._page-toolbar')
                         </div>
 
                         @yield('content')
                     </div>
                 </div>
 
-                @include('layouts.admin-1._quick-sidebar')
+                @include('layouts.admin-1-minimal._quick-sidebar')
             </div>
 
-            @include('layouts.admin-1._page-footer')
+            @include('layouts.admin-1-minimal._page-footer')
         </div>
 
-        @include('layouts.admin-1._quick-nav')
+        @include('layouts.admin-1-minimal._quick-nav')
 
-        @include('layouts.admin-1._footer')
+        @include('layouts.admin-1-minimal._footer')
 
         @stack('footer-script')
     </body>
