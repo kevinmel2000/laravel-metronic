d8ccdbb8d25c62e56dd75178884cd674605e557a
R. Kukuh <rkukuh@gmail.com>
2017-06-22 21:33:33 +0700

    Delete unused partial view for minimal layout

--- resources/views/layouts/admin-1-minimal/navbar/quick-sidebar.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
-<li class="dropdown dropdown-quick-sidebar-toggler">
-    <a href="javascript:;" class="dropdown-toggle">
-        <i class="icon-logout"></i>
-    </a>
-</li>
