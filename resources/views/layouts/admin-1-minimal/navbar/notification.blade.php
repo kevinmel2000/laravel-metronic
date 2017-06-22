91d79e5ec6b4673106d0dd232a3352ef7834d3e0
R. Kukuh <rkukuh@gmail.com>
2017-06-22 21:38:08 +0700

    Simplify template content

--- resources/views/layouts/admin-1-minimal/navbar/notification.blade.php
+++ resources/views/layouts/admin-1-minimal/navbar/notification.blade.php
@@ -32,69 +32,6 @@
                             </span> Server #12 overloaded. </span>
                     </a>
                 </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">10 mins</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-warning">
-                                <i class="fa fa-bell-o"></i>
-                            </span> Server #2 not responding. </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">14 hrs</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-info">
-                                <i class="fa fa-bullhorn"></i>
-                            </span> Application error. </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">2 days</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-danger">
-                                <i class="fa fa-bolt"></i>
-                            </span> Database overloaded 68%. </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">3 days</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-danger">
-                                <i class="fa fa-bolt"></i>
-                            </span> A user IP blocked. </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">4 days</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-warning">
-                                <i class="fa fa-bell-o"></i>
-                            </span> Storage Server #4 not responding dfdfdfd. </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">5 days</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-info">
-                                <i class="fa fa-bullhorn"></i>
-                            </span> System Error. </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript:;">
-                        <span class="time">9 days</span>
-                        <span class="details">
-                            <span class="label label-sm label-icon label-danger">
-                                <i class="fa fa-bolt"></i>
-                            </span> Storage server failed. </span>
-                    </a>
-                </li>
             </ul>
         </li>
     </ul>
