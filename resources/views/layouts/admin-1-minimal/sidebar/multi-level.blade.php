c9541ab5e6fc44247f426e5c86dbf43cdde0c136
R. Kukuh <rkukuh@gmail.com>
2017-06-24 23:06:28 +0700

    Typo

--- resources/views/layouts/admin-1-minimal/sidebar/multi-level.blade.php
+++ resources/views/layouts/admin-1-minimal/sidebar/multi-level.blade.php
@@ -47,7 +47,7 @@
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">
-                <i class="icon-bar-chart"></i> Item 3 </a>
+                <i class="icon-bar-chart"></i> Item 2 </a>
         </li>
     </ul>
 </li>
