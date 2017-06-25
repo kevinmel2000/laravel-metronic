8d08cb97615e7be0e222ded16f6ca89c73d84347
R. Kukuh <rkukuh@gmail.com>
2017-06-25 23:44:29 +0700

    Use '#' as link's href value

--- resources/views/layouts/admin-1-minimal/sidebar/page-layout.blade.php
+++ resources/views/layouts/admin-1-minimal/sidebar/page-layout.blade.php
@@ -11,27 +11,27 @@
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_search_on_header_1.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Search Box On Header 1</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_search_on_header_2.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Search Box On Header 2</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_language_bar.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Header Language Bar</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_footer_fixed.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Fixed Footer</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_boxed_page.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Boxed Page</span>
             </a>
         </li>
