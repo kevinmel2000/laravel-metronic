8fdcde5b06db3b6ea0aeca43806e7f1795c7f772
R. Kukuh <rkukuh@gmail.com>
2017-06-25 16:47:43 +0700

    Use '>' instead of '/>'

--- resources/views/admin-1/pages/user-login-2.blade.php
+++ resources/views/admin-1/pages/user-login-2.blade.php
@@ -6,40 +6,40 @@
     <!--<![endif]-->
     <!-- BEGIN HEAD -->
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
         <title>Metronic Admin Theme #1 | User Login 2</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content="Preview page of Metronic Admin Theme #1 for " name="description" />
-        <meta content="" name="author" />
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta content="Preview page of Metronic Admin Theme #1 for " name="description">
+        <meta content="" name="author">
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
+        <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css">
+        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css">
         <!-- END GLOBAL MANDATORY STYLES -->
         <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <link href="../assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css">
+        <link href="../assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css">
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+        <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css">
+        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css">
         <!-- END THEME GLOBAL STYLES -->
         <!-- BEGIN PAGE LEVEL STYLES -->
-        <link href="../assets/pages/css/login-2.min.css" rel="stylesheet" type="text/css" />
+        <link href="../assets/pages/css/login-2.min.css" rel="stylesheet" type="text/css">
         <!-- END PAGE LEVEL STYLES -->
         <!-- BEGIN THEME LAYOUT STYLES -->
         <!-- END THEME LAYOUT STYLES -->
-        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="shortcut icon" href="favicon.ico">
     </head>
     <!-- END HEAD -->
     <body class=" login">
         <!-- BEGIN LOGO -->
         <div class="logo">
             <a href="index.html">
-                <img src="../assets/pages/img/logo-big-white.png" style="height: 17px;" alt="" /> </a>
+                <img src="../assets/pages/img/logo-big-white.png" style="height: 17px;" alt=""> </a>
         </div>
         <!-- END LOGO -->
         <!-- BEGIN LOGIN -->
@@ -57,17 +57,17 @@
                 <div class="form-group">
                     <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
                     <label class="control-label visible-ie8 visible-ie9">Username</label>
-                    <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username" /> </div>
+                    <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"> </div>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Password</label>
-                    <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password" /> </div>
+                    <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"> </div>
                 <div class="form-actions">
                     <button type="submit" class="btn red btn-block uppercase">Login</button>
                 </div>
                 <div class="form-actions">
                     <div class="pull-left">
                         <label class="rememberme mt-checkbox mt-checkbox-outline">
-                            <input type="checkbox" name="remember" value="1" /> Remember me
+                            <input type="checkbox" name="remember" value="1"> Remember me
                             <span></span>
                         </label>
                     </div>
@@ -106,7 +106,7 @@
                     <span class="form-subtitle">Enter your e-mail to reset it.</span>
                 </div>
                 <div class="form-group">
-                    <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email" /> </div>
+                    <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email"> </div>
                 <div class="form-actions">
                     <button type="button" id="back-btn" class="btn btn-default">Back</button>
                     <button type="submit" class="btn btn-primary uppercase pull-right">Submit</button>
@@ -121,17 +121,17 @@
                 <p class="hint"> Enter your personal details below: </p>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Full Name</label>
-                    <input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname" /> </div>
+                    <input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname"> </div>
                 <div class="form-group">
                     <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
                     <label class="control-label visible-ie8 visible-ie9">Email</label>
-                    <input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email" /> </div>
+                    <input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email"> </div>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Address</label>
-                    <input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address" /> </div>
+                    <input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address"> </div>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">City/Town</label>
-                    <input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city" /> </div>
+                    <input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city"> </div>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Country</label>
                     <select name="country" class="form-control">
@@ -375,16 +375,16 @@
                 <p class="hint"> Enter your account details below: </p>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Username</label>
-                    <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username" /> </div>
+                    <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"> </div>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Password</label>
-                    <input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password" /> </div>
+                    <input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password"> </div>
                 <div class="form-group">
                     <label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
-                    <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="rpassword" /> </div>
+                    <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="rpassword"> </div>
                 <div class="form-group margin-top-20 margin-bottom-20">
                     <label class="mt-checkbox mt-checkbox-outline">
-                        <input type="checkbox" name="tnc" /> I agree to the
+                        <input type="checkbox" name="tnc"> I agree to the
                         <a href="javascript:;">Terms of Service </a> &
                         <a href="javascript:;">Privacy Policy </a>
                         <span></span>
