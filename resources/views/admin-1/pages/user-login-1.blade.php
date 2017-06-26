f5b9a9e096efba32e50bcb6a6a54cc446d8b0d64
R. Kukuh <rkukuh@gmail.com>
2017-06-26 16:17:28 +0700

    Syntax styling

--- resources/views/admin-1/pages/user-login-1.blade.php
+++ resources/views/admin-1/pages/user-login-1.blade.php
@@ -385,28 +385,28 @@
         </div>
         <div class="copyright"> 2014 © Metronic. Admin Dashboard Template. </div>
         <!--[if lt IE 9]>
-<script src="{{ asset('assets/global/plugins/respond.min.js') }}"></script>
-<script src="{{ asset('assets/global/plugins/excanvas.min.js') }}"></script>
-<script src="{{ asset('assets/global/plugins/ie8.fix.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/respond.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/excanvas.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/global/plugins/ie8.fix.min.js') }}"></script>
 <![endif]-->
         <!-- BEGIN CORE PLUGINS -->
-        <script src="{{ asset('assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/js.cookie.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.blockui.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
         <!-- END CORE PLUGINS -->
         <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <script src="{{ asset('assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('assets/global/plugins/select2/js/select2.full.min.js') }}" type="text/javascript"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/plugins/select2/js/select2.full.min.js') }}"></script>
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL SCRIPTS -->
-        <script src="{{ asset('assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
+        <script type="text/javascript" src="{{ asset('assets/global/scripts/app.min.js') }}"></script>
         <!-- END THEME GLOBAL SCRIPTS -->
         <!-- BEGIN PAGE LEVEL SCRIPTS -->
-        <script src="{{ asset('assets/pages/scripts/login.min.js') }}" type="text/javascript"></script>
+        <script type="text/javascript" src="{{ asset('assets/pages/scripts/login.min.js') }}"></script>
         <!-- END PAGE LEVEL SCRIPTS -->
         <!-- BEGIN THEME LAYOUT SCRIPTS -->
         <!-- END THEME LAYOUT SCRIPTS -->
