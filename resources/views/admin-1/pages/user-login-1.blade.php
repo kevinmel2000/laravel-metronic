dcdfc9c4c7a8b8b16ef849f73e592bb45d1a67f3
R. Kukuh <rkukuh@gmail.com>
2017-06-26 16:12:17 +0700

    Syntax styling

--- resources/views/admin-1/pages/user-login-1.blade.php
+++ resources/views/admin-1/pages/user-login-1.blade.php
@@ -13,26 +13,27 @@
         <meta content="Preview page of Metronic Admin Theme #1 for " name="description">
         <meta content="" name="author">
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css">
         <!-- END GLOBAL MANDATORY STYLES -->
         <!-- BEGIN PAGE LEVEL PLUGINS -->
-        <link href="../assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css">
-        <link href="../assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/select2/css/select2.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/plugins/select2/css/select2-bootstrap.min.css">
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL STYLES -->
-        <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css">
-        <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../assets/global/css/components.min.css" id="style_components">
+        <link rel="stylesheet" type="text/css" href="../assets/global/css/plugins.min.css">
         <!-- END THEME GLOBAL STYLES -->
         <!-- BEGIN PAGE LEVEL STYLES -->
-        <link href="../assets/pages/css/login.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../assets/pages/css/login.min.css">
         <!-- END PAGE LEVEL STYLES -->
         <!-- BEGIN THEME LAYOUT STYLES -->
         <!-- END THEME LAYOUT STYLES -->
-        <link rel="shortcut icon" href="favicon.ico"> </head>
+        <link rel="stylesheet" type="text/css" rel="shortcut icon" href="favicon.ico">
+    </head>
     <!-- END HEAD -->
     <body class=" login">
         <!-- BEGIN LOGO -->
