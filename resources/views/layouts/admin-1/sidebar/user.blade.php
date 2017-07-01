629c96c3a30922664a10ca30a666b1174d59874f
R. Kukuh <rkukuh@gmail.com>
2017-07-01 17:30:21 +0700

    Apply named route to anchor link

--- resources/views/layouts/admin-1/sidebar/user.blade.php
+++ resources/views/layouts/admin-1/sidebar/user.blade.php
@@ -46,7 +46,7 @@
                     <a href="{{ route('admin-1.page-user-login-3') }}" class="nav-link " target="_blank"> Login Page 3 </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="page_user_login_4.html" class="nav-link " target="_blank"> Login Page 4 </a>
+                    <a href="{{ route('admin-1.page-user-login-4') }}" class="nav-link " target="_blank"> Login Page 4 </a>
                 </li>
                 <li class="nav-item ">
                     <a href="page_user_login_5.html" class="nav-link " target="_blank"> Login Page 5 </a>
