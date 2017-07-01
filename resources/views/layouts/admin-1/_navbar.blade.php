29acf3e3588cecdb91b3f1bda86379e2275988ca
R. Kukuh <rkukuh@gmail.com>
2017-07-02 06:50:09 +0700

    Change page logo's url

--- resources/views/layouts/admin-1/_navbar.blade.php
+++ resources/views/layouts/admin-1/_navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="page-header-inner ">
 
         <div class="page-logo">
-            <a href="index.html">
+            <a href="{{ route('admin.index') }}">
                 <img src="{{ asset('assets/layouts/layout/img/logo.png') }}" alt="logo" class="logo-default">
             </a>
             <div class="menu-toggler sidebar-toggler">
