a8049bfad9cb7edacce96859d7485fd2ca3e8bd7
R. Kukuh <rkukuh@gmail.com>
2017-07-01 17:24:46 +0700

    Fix: Incorrect file path

--- public/assets/pages/scripts/login.js
+++ public/assets/pages/scripts/login.js
@@ -27,7 +27,7 @@ var Login = function() {
                 }
             },
 
-            invalidHandler: function(event, validator) { //display error alert on form submit   
+            invalidHandler: function(event, validator) { //display error alert on form submit
                 $('.alert-danger', $('.login-form')).show();
             },
 
@@ -79,7 +79,7 @@ var Login = function() {
                 }
             },
 
-            invalidHandler: function(event, validator) { //display error alert on form submit   
+            invalidHandler: function(event, validator) { //display error alert on form submit
 
             },
 
@@ -128,9 +128,9 @@ var Login = function() {
         function format(state) {
             if (!state.id) { return state.text; }
             var $state = $(
-             '<span><img src="../assets/global/img/flags/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
+             '<span><img src="../../assets/global/img/flags/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
             );
-            
+
             return $state;
         }
 
@@ -139,7 +139,7 @@ var Login = function() {
 	            placeholder: '<i class="fa fa-map-marker"></i>&nbsp;Select a Country',
 	            templateResult: format,
                 templateSelection: format,
-                width: 'auto', 
+                width: 'auto',
 	            escapeMarkup: function(m) {
 	                return m;
 	            }
@@ -196,7 +196,7 @@ var Login = function() {
                 }
             },
 
-            invalidHandler: function(event, validator) { //display error alert on form submit   
+            invalidHandler: function(event, validator) { //display error alert on form submit
 
             },
 
@@ -211,7 +211,7 @@ var Login = function() {
             },
 
             errorPlacement: function(error, element) {
-                if (element.attr("name") == "tnc") { // insert checkbox errors after the container                  
+                if (element.attr("name") == "tnc") { // insert checkbox errors after the container
                     error.insertAfter($('#register_tnc_error'));
                 } else if (element.closest('.input-icon').size() === 1) {
                     error.insertAfter(element.closest('.input-icon'));
