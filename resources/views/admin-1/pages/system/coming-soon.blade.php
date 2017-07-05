a5fe54d56b5c60060e3293d34a8bbe9542d6ebac
R. Kukuh <rkukuh@gmail.com>
2017-07-06 00:12:13 +0700

    Update page title

--- resources/views/admin-1/pages/system/coming-soon.blade.php
+++ resources/views/admin-1/pages/system/coming-soon.blade.php
@@ -9,7 +9,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }} | Coming Soon</title>
+        <title>{{ config('app.name', 'Laravel') }} | Admin-1 | Coming Soon</title>
 
         <meta name="description" content="Integration between Laravel framework and Metronic template">
         <meta name="author" content="R. Kukuh">
