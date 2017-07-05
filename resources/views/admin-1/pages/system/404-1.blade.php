ecfeb4d1d3e5f10904aec47d3aa9012b75120b74
R. Kukuh <rkukuh@gmail.com>
2017-07-05 23:43:40 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/system/404-1.blade.php
+++ resources/views/admin-1/pages/system/404-1.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Admin-1 404 #1')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>System</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
