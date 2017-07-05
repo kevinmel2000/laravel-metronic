b237fb1afab71f0c3764fa7403e5eb8ee4fe5df8
R. Kukuh <rkukuh@gmail.com>
2017-07-06 00:15:57 +0700

    Add @stack directives

--- resources/views/layouts/admin-1/_footer.blade.php
+++ resources/views/layouts/admin-1/_footer.blade.php
@@ -13,9 +13,17 @@
 <script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
 <!-- END CORE PLUGINS -->
 
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+@stack('page-level-plugins-footer')
+<!-- END PAGE LEVEL PLUGINS -->
+
 <!-- BEGIN THEME GLOBAL SCRIPTS -->
 <script type="text/javascript" src="{{ asset('assets/global/scripts/app.min.js') }}"></script>
 <!-- END THEME GLOBAL SCRIPTS -->
+
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+@stack('page-level-scripts')
+<!-- END PAGE LEVEL SCRIPTS -->
 
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
 <script type="text/javascript" src="{{ asset('assets/layouts/layout/scripts/layout.min.js') }}"></script>
