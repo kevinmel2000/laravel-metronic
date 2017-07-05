7be51e67702c5a24ef4c651159b334477f586595
R. Kukuh <rkukuh@gmail.com>
2017-07-05 23:46:05 +0700

    Add content

--- resources/views/admin-1/pages/system/404-1.blade.php
+++ resources/views/admin-1/pages/system/404-1.blade.php
@@ -15,4 +15,31 @@
 @endsection
 
 @section('content')
+    <h1 class="page-title">
+        404 Page Option 1
+        <small>404 page option 1</small>
+    </h1>
+
+    <div class="row">
+        <div class="col-md-12 page-404">
+            <div class="number font-green"> 404 </div>
+            <div class="details">
+                <h3>Oops! You're lost.</h3>
+                <p> We can not find the page you're looking for.<br>
+                    <a href="index.html"> Return home </a> or try the search bar below.</p>
+
+                <form action="#">
+                    <div class="input-group input-medium">
+                        <input type="text" class="form-control" placeholder="keyword...">
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn green">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
+    </div>
 @endsection
