f5b90524da8b48e360c76e657d4e1f97df977a95
R. Kukuh <rkukuh@gmail.com>
2017-07-07 19:21:45 +0700

    Fix: Incorrect anchor link href value

--- resources/views/admin-1/pages/system/404-3.blade.php
+++ resources/views/admin-1/pages/system/404-3.blade.php
@@ -47,7 +47,7 @@
             <h1>404</h1>
             <h2>Houston, we have a problem.</h2>
             <p> Actually, the page you are looking for does not exist. </p>
-            <p><a href="index.html" class="btn red btn-outline"> Return home </a></p>
+            <p><a href="{{ route('admin-1.index') }}" class="btn red btn-outline"> Return home </a></p>
         </div>
 
         <!--[if lt IE 9]>
