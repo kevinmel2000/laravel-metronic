9d912b329454d188eed7f3b69d7dbf575e651da0
R. Kukuh <rkukuh@gmail.com>
2017-07-07 19:34:28 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/system/500-1.blade.php
+++ resources/views/admin-1/pages/system/500-1.blade.php
@@ -3,6 +3,15 @@
 @section('title', '500 #1')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>System</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
