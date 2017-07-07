733bca3ed1bf72ac39bcfec08a56ede02770a057
R. Kukuh <rkukuh@gmail.com>
2017-07-07 19:35:45 +0700

    Add content

--- resources/views/admin-1/pages/system/500-1.blade.php
+++ resources/views/admin-1/pages/system/500-1.blade.php
@@ -15,4 +15,21 @@
 @endsection
 
 @section('content')
+    <h1 class="page-title"> 500 Page Option 1
+        <small>500 page option 1</small>
+    </h1>
+
+    <div class="row">
+        <div class="col-md-12 page-500">
+            <div class=" number font-red"> 500 </div>
+            <div class=" details">
+                <h3>Oops! Something went wrong.</h3>
+                <p> We are fixing it! Please come back in a while.
+                    <br/> </p>
+                <p>
+                    <a href="index.html" class="btn red btn-outline"> Return home </a>
+                    <br> </p>
+            </div>
+        </div>
+    </div>
 @endsection
