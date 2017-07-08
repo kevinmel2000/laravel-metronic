81d60af2b152a6935b4daec51dfa2c65a3bb7a28
R. Kukuh <rkukuh@gmail.com>
2017-07-08 21:18:32 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/general/about.blade.php
+++ resources/views/admin-1/pages/general/about.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'About')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>General</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
