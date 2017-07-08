00205b1d7e477616861a242e719d61c681ed9f2c
R. Kukuh <rkukuh@gmail.com>
2017-07-08 21:20:15 +0700

    Update content

--- resources/views/admin-1/pages/general/about.blade.php
+++ resources/views/admin-1/pages/general/about.blade.php
@@ -30,4 +30,65 @@
         </div>
     </div>
     <!-- END CONTENT HEADER -->
+
+    <!-- BEGIN CARDS -->
+    <div class="row margin-bottom-20">
+        <div class="col-lg-3 col-md-6">
+            <div class="portlet light">
+                <div class="card-icon">
+                    <i class="icon-user-follow font-red-sunglo theme-font"></i>
+                </div>
+                <div class="card-title">
+                    <span> Best User Expierence </span>
+                </div>
+                <div class="card-desc">
+                    <span> The best way to find yourself is
+                        <br> to lose yourself in the service of others </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="portlet light">
+                <div class="card-icon">
+                    <i class="icon-trophy font-green-haze theme-font"></i>
+                </div>
+                <div class="card-title">
+                    <span> Awards Winner </span>
+                </div>
+                <div class="card-desc">
+                    <span> The best way to find yourself is
+                        <br> to lose yourself in the service of others </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="portlet light">
+                <div class="card-icon">
+                    <i class="icon-basket font-purple-wisteria theme-font"></i>
+                </div>
+                <div class="card-title">
+                    <span> eCommerce Components </span>
+                </div>
+                <div class="card-desc">
+                    <span> The best way to find yourself is
+                        <br> to lose yourself in the service of others </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="portlet light">
+                <div class="card-icon">
+                    <i class="icon-layers font-blue theme-font"></i>
+                </div>
+                <div class="card-title">
+                    <span> Adaptive Components </span>
+                </div>
+                <div class="card-desc">
+                    <span> The best way to find yourself is
+                        <br> to lose yourself in the service of others </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END CARDS -->
 @endsection
