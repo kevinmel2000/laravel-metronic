eba5b444dca5499c6d6c5edb851da1460a78a24a
R. Kukuh <rkukuh@gmail.com>
2017-07-08 21:06:03 +0700

    Fill content

--- resources/views/admin-1/pages/system/cookie-consent-2.blade.php
+++ resources/views/admin-1/pages/system/cookie-consent-2.blade.php
@@ -15,4 +15,27 @@
 @endsection
 
 @section('content')
+    <h1 class="page-title"> Cookie Consent 2
+        <small>Cookie Consent Demo</small>
+    </h1>
+
+    <div class="note note-success">
+        <h3>Cookie Consent Bar</h3>
+        <p> A simple, lightweight jQuery plugin for creating a notification bar that is dismissable and dismiss is saved by cookie. Perfect for implementing the new eu cookielaw. For more info check
+            <a class="btn red btn-outline" href="https://github.com/carlwoodhouse/jquery.cookieBar"
+                target="_blank">the official documentation</a>
+        </p>
+    </div>
+    
+    <div class="mt-cookie-consent-bar mt-cookie-consent-bar-light">
+        <div class="mt-cookie-consent-bar-holder">
+            <div class="mt-cookie-consent-bar-content"> This website uses cookies to ensure you get the best experience on our website.
+                <a href="http://www.allaboutcookies.org/cookies/" tatget="_blank">What is cookie ?</a>
+                <a href="#">Our Cookie Policy</a>
+            </div>
+            <div class="mt-cookie-consent-bar-action">
+                <a href="javascript:;" class="mt-cookie-consent-btn btn btn-circle red">Understand</a>
+            </div>
+        </div>
+    </div>
 @endsection
