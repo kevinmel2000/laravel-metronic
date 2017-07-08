dd1466dffbdd9a020789fa59323919e736e29109
R. Kukuh <rkukuh@gmail.com>
2017-07-08 21:19:33 +0700

    Construct a content

--- resources/views/admin-1/pages/general/about.blade.php
+++ resources/views/admin-1/pages/general/about.blade.php
@@ -15,4 +15,19 @@
 @endsection
 
 @section('content')
+    <!-- BEGIN PAGE TITLE-->
+    <h1 class="page-title"> About Us
+        <small>about us page</small>
+    </h1>
+    <!-- END PAGE TITLE-->
+
+    <!-- BEGIN CONTENT HEADER -->
+    <div class="row margin-bottom-40 about-header">
+        <div class="col-md-12">
+            <h1>About Us</h1>
+            <h2>Life is either a great adventure or nothing</h2>
+            <button type="button" class="btn btn-danger">JOIN US TODAY</button>
+        </div>
+    </div>
+    <!-- END CONTENT HEADER -->
 @endsection
