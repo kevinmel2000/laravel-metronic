7641c8f0a6902b6894997e66d80b9ac5e45d7f79
R. Kukuh <rkukuh@gmail.com>
2017-07-10 05:48:19 +0700

    Fix: No '../' prefix on image path

--- resources/views/admin-1/pages/general/portfolio-1.blade.php
+++ resources/views/admin-1/pages/general/portfolio-1.blade.php
@@ -45,12 +45,12 @@
             <div class="cbp-item graphic">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/013.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/013.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
-                                <a href="../assets/global/img/portfolio/1200x900/57.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="Dashboard<br>by Paul Flavius Nechita">view larger</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/img/portfolio/1200x900/57.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="Dashboard<br>by Paul Flavius Nechita">view larger</a>
                             </div>
                         </div>
                     </div>
@@ -61,12 +61,12 @@
             <div class="cbp-item web-design logos">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/05.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/05.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
-                                <a href="../assets/global/img/portfolio/1200x900/50.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="World Clock Widget<br>by Paul Flavius Nechita">view larger</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/img/portfolio/1200x900/50.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="World Clock Widget<br>by Paul Flavius Nechita">view larger</a>
                             </div>
                         </div>
                     </div>
@@ -77,11 +77,11 @@
             <div class="cbp-item graphic logos">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/16.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/16.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
                                 <a href="http://vimeo.com/14912890" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="To-Do Dashboard<br>by Tiberiu Neamu">view video</a>
                             </div>
                         </div>
@@ -93,11 +93,11 @@
             <div class="cbp-item web-design graphic">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/33.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/33.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
                                 <a href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/4900333&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
                                     class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="Events and  More<br>by Tiberiu Neamu">view sound</a>
                             </div>
@@ -110,12 +110,12 @@
             <div class="cbp-item identity web-design">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/38.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/38.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
-                                <a href="../assets/global/img/portfolio/1200x900/4.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="WhereTO App<br>by Tiberiu Neamu">view larger</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/img/portfolio/1200x900/4.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="WhereTO App<br>by Tiberiu Neamu">view larger</a>
                             </div>
                         </div>
                     </div>
@@ -126,12 +126,12 @@
             <div class="cbp-item identity web-design">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/88.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/88.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
-                                <a href="../assets/global/img/portfolio/1200x900/7.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="Ski * Buddy<br>by Tiberiu Neamu">view larger</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/img/portfolio/1200x900/7.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase btn red uppercase" data-title="Ski * Buddy<br>by Tiberiu Neamu">view larger</a>
                             </div>
                         </div>
                     </div>
@@ -142,12 +142,12 @@
             <div class="cbp-item graphic logos">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/02.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/02.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
-                                <a href="../assets/global/img/portfolio/1200x900/60.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Seemple* Music for iPad<br>by Tiberiu Neamu">view sound</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/img/portfolio/1200x900/60.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Seemple* Music for iPad<br>by Tiberiu Neamu">view sound</a>
                             </div>
                         </div>
                     </div>
@@ -158,11 +158,11 @@
             <div class="cbp-item identity graphic">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/62.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/62.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
                                 <a href="http://www.youtube.com/watch?v=Bu9OiDmxCrQ" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Remind~Me More<br>by Tiberiu Neamu">view video</a>
                             </div>
                         </div>
@@ -174,11 +174,11 @@
             <div class="cbp-item web-design graphic">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/81.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/81.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
                                 <a href="https://www.ted.com/talks/andrew_bastawrous_get_your_next_eye_exam_on_a_smartphone" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Workout Buddy<br>by Tiberiu Neamu">view video</a>
                             </div>
                         </div>
@@ -190,11 +190,11 @@
             <div class="cbp-item identity web-design">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/87.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/87.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
                                 <a href="https://www.youtube.com/watch?v=3wbvpOIIBQA" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Bills Bills Bills<br>by Cosmin Capitanu">view video</a>
                             </div>
                         </div>
@@ -206,12 +206,12 @@
             <div class="cbp-item identity logos">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/102.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/102.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
-                                <a href="../assets/global/img/portfolio/1200x900/92.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Generic Apps<br>by Cosmin Capitanu">view video</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project1.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/img/portfolio/1200x900/92.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Generic Apps<br>by Cosmin Capitanu">view video</a>
                             </div>
                         </div>
                     </div>
@@ -222,11 +222,11 @@
             <div class="cbp-item graphic web-design">
                 <div class="cbp-caption">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="../assets/global/img/portfolio/600x600/96.jpg" alt=""> </div>
+                        <img src="assets/global/img/portfolio/600x600/96.jpg" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignCenter">
                             <div class="cbp-l-caption-body">
-                                <a href="../assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
+                                <a href="assets/global/plugins/cubeportfolio/ajax/project2.html" class="cbp-singlePage cbp-l-caption-buttonLeft btn red uppercase" rel="nofollow">more info</a>
                                 <a href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/26519543&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
                                     class="cbp-lightbox cbp-l-caption-buttonRight btn red uppercase" data-title="Speed Detector<br>by Cosmin Capitanu">view sound</a>
                             </div>
@@ -238,7 +238,7 @@
             </div>
         </div>
         <div id="js-loadMore-juicy-projects" class="cbp-l-loadMore-button">
-            <a href="../assets/global/plugins/cubeportfolio/ajax/loadMore.html" class="cbp-l-loadMore-link btn grey-mint btn-outline" rel="nofollow">
+            <a href="assets/global/plugins/cubeportfolio/ajax/loadMore.html" class="cbp-l-loadMore-link btn grey-mint btn-outline" rel="nofollow">
                 <span class="cbp-l-loadMore-defaultText">LOAD MORE</span>
                 <span class="cbp-l-loadMore-loadingText">LOADING...</span>
                 <span class="cbp-l-loadMore-noMoreLoading">NO MORE WORKS</span>
