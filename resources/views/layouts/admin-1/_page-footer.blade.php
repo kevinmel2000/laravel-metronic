4103da7a0ff94de1e56737e6ce04db3c3789f85c
R. Kukuh <rkukuh@gmail.com>
2017-07-10 06:04:37 +0700

    Use view shared data instead

--- resources/views/layouts/admin-1/_page-footer.blade.php
+++ resources/views/layouts/admin-1/_page-footer.blade.php
@@ -1,6 +1,6 @@
 <div class="page-footer">
     <div class="page-footer-inner">
-        {{ \Carbon\Carbon::now()->format('Y') }} &copy; R.Kukuh
+        {{ $now->format('Y') }} &copy; R.Kukuh
     </div>
     <div class="scroll-to-top">
         <i class="icon-arrow-up"></i>
