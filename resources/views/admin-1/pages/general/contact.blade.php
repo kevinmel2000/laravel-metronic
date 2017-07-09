95f85fc4523c66cb2dcca8f4942500e87d4e1127
R. Kukuh <rkukuh@gmail.com>
2017-07-09 22:15:33 +0700

    Add 'Goole map' to content

--- resources/views/admin-1/pages/general/contact.blade.php
+++ resources/views/admin-1/pages/general/contact.blade.php
@@ -20,4 +20,57 @@
         <small>contact us page</small>
     </h1>
     <!-- END PAGE TITLE-->
+
+    <div class="c-content-contact-1 c-opt-1">
+        <div class="row" data-auto-height=".c-height">
+            <div class="col-lg-8 col-md-6 c-desktop"></div>
+            <div class="col-lg-4 col-md-6">
+                <div class="c-body">
+                    <div class="c-section">
+                        <h3>Metronic Inc.</h3>
+                    </div>
+                    <div class="c-section">
+                        <div class="c-content-label uppercase bg-blue">Address</div>
+                        <p>25, Lorem Lis Street,
+                            <br/>Orange C, California,
+                            <br/>United States of America</p>
+                    </div>
+                    <div class="c-section">
+                        <div class="c-content-label uppercase bg-blue">Contacts</div>
+                        <p>
+                            <strong>T</strong> 800 123 0000
+                            <br/>
+                            <strong>F</strong> 800 123 8888</p>
+                    </div>
+                    <div class="c-section">
+                        <div class="c-content-label uppercase bg-blue">Social</div>
+                        <br/>
+                        <ul class="c-content-iconlist-1 ">
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-youtube-play"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="gmapbg" class="c-content-contact-1-gmap" style="height: 615px;"></div>
+    </div>
 @endsection
