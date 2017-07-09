9d469b75c64a57cfd603303b2f5f804a3954c58c
R. Kukuh <rkukuh@gmail.com>
2017-07-09 22:16:40 +0700

    Add 'Contact form' to content

--- resources/views/admin-1/pages/general/contact.blade.php
+++ resources/views/admin-1/pages/general/contact.blade.php
@@ -71,4 +71,55 @@
         </div>
         <div id="gmapbg" class="c-content-contact-1-gmap" style="height: 615px;"></div>
     </div>
+
+    <div class="c-content-feedback-1 c-option-1">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="c-container bg-green">
+                    <div class="c-content-title-1 c-inverse">
+                        <h3 class="uppercase">Need to know more?</h3>
+                        <div class="c-line-left"></div>
+                        <p class="c-font-lowercase">Try visiting our FAQ page to learn more about our greatest ever expanding theme, Metronic.</p>
+                        <button class="btn grey-cararra font-dark">Learn More</button>
+                    </div>
+                </div>
+                <div class="c-container bg-grey-steel">
+                    <div class="c-content-title-1">
+                        <h3 class="uppercase">Have a question?</h3>
+                        <div class="c-line-left bg-dark"></div>
+                        <form action="#">
+                            <div class="input-group input-group-lg c-square">
+                                <input type="text" class="form-control c-square" placeholder="Ask a question" />
+                                <span class="input-group-btn">
+                                    <button class="btn uppercase" type="button">Go!</button>
+                                </span>
+                            </div>
+                        </form>
+                        <p>Ask your questions away and let our dedicated customer service help you look through our FAQs to get your questions answered!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="c-contact">
+                    <div class="c-content-title-1">
+                        <h3 class="uppercase">Keep in touch</h3>
+                        <div class="c-line-left bg-dark"></div>
+                        <p class="c-font-lowercase">Our helpline is always open to receive any inquiry or feedback. Please feel free to drop us an email from the form below and we will get back to you as soon as we can.</p>
+                    </div>
+                    <form action="#">
+                        <div class="form-group">
+                            <input type="text" placeholder="Your Name" class="form-control input-md"> </div>
+                        <div class="form-group">
+                            <input type="text" placeholder="Your Email" class="form-control input-md"> </div>
+                        <div class="form-group">
+                            <input type="text" placeholder="Contact Phone" class="form-control input-md"> </div>
+                        <div class="form-group">
+                            <textarea rows="8" name="message" placeholder="Write comment here ..." class="form-control input-md"></textarea>
+                        </div>
+                        <button type="submit" class="btn grey">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
