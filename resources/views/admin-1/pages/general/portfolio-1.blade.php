c6c8e0876c431b232cad693a64a34f0506b94f1d
R. Kukuh <rkukuh@gmail.com>
2017-07-10 05:57:46 +0700

    Push external CSS file

--- resources/views/admin-1/pages/general/portfolio-1.blade.php
+++ resources/views/admin-1/pages/general/portfolio-1.blade.php
@@ -246,3 +246,7 @@
         </div>
     </div>
 @endsection
+
+@push('page-level-plugins-header')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/cubeportfolio/css/cubeportfolio.css') }}">
+@endpush
