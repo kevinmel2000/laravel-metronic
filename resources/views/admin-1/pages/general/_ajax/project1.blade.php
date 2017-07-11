c6b159d0e88b004d973454f523f217b5c5f7f5f7
R. Kukuh <rkukuh@gmail.com>
2017-07-11 18:21:37 +0700

    Use asset() helper to pull local assets

--- resources/views/admin-1/pages/general/_ajax/project1.blade.php
+++ resources/views/admin-1/pages/general/_ajax/project1.blade.php
@@ -8,8 +8,8 @@
                     <img src="{{ asset('assets/global/img/portfolio/1200x900/06.jpg') }}" alt=""> </a>
             </li>
             <li class="cbp-slider-item">
-                <a href="assets/global/img/portfolio/1200x900/08.jpg" class="cbp-lightbox">
-                    <img src="assets/global/img/portfolio/1200x900/08.jpg" alt=""> </a>
+                <a href="{{ asset('assets/global/img/portfolio/1200x900/08.jpg') }}" class="cbp-lightbox">
+                    <img src="{{ asset('assets/global/img/portfolio/1200x900/08.jpg') }}" alt=""> </a>
             </li>
             <li class="cbp-slider-item">
                 <a href="assets/global/img/portfolio/1200x900/77.jpg" class="cbp-lightbox">
