3f2bc78cf8b7bd182ff782519d4b24c9a28d553f
R. Kukuh <rkukuh@gmail.com>
2017-07-14 17:34:59 +0700

    Use asset() and route() helper instead

--- resources/views/admin-1/pages/general/portfolio-3.blade.php
+++ resources/views/admin-1/pages/general/portfolio-3.blade.php
@@ -64,9 +64,9 @@
                 </a>
             </div>
             <div class="cbp-item web-design logos identity graphic">
-                <a href="assets/global/plugins/cubeportfolio/ajax/project4.html" class="cbp-caption cbp-singlePageInline" data-title="Bolt UI<br>by Tiberiu Neamu" rel="nofollow">
+                <a href="{{ route('admin-1.ajax.project4') }}" class="cbp-caption cbp-singlePageInline" data-title="Bolt UI<br>by Tiberiu Neamu" rel="nofollow">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="assets/global/img/portfolio/600x600/1.jpg" alt=""> </div>
+                        <img src="{{ asset('assets/global/img/portfolio/600x600/1.jpg') }}" alt=""> </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignLeft">
                             <div class="cbp-l-caption-body">
