402462b1e95082244d8e6599a94e8563a31b6906
R. Kukuh <rkukuh@gmail.com>
2017-07-14 17:35:22 +0700

    Syntax styling

--- resources/views/admin-1/pages/general/portfolio-3.blade.php
+++ resources/views/admin-1/pages/general/portfolio-3.blade.php
@@ -66,7 +66,8 @@
             <div class="cbp-item web-design logos identity graphic">
                 <a href="{{ route('admin-1.ajax.project4') }}" class="cbp-caption cbp-singlePageInline" data-title="Bolt UI<br>by Tiberiu Neamu" rel="nofollow">
                     <div class="cbp-caption-defaultWrap">
-                        <img src="{{ asset('assets/global/img/portfolio/600x600/1.jpg') }}" alt=""> </div>
+                        <img src="{{ asset('assets/global/img/portfolio/600x600/1.jpg') }}" alt="">
+                    </div>
                     <div class="cbp-caption-activeWrap">
                         <div class="cbp-l-caption-alignLeft">
                             <div class="cbp-l-caption-body">
