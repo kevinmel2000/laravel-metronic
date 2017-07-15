2624607de9152b73ea55ecbfb344d9f2a4f1d175
R. Kukuh <rkukuh@gmail.com>
2017-07-16 05:37:31 +0700

    Set container background color

--- resources/views/admin-1/pages/general/search-5.blade.php
+++ resources/views/admin-1/pages/general/search-5.blade.php
@@ -326,3 +326,9 @@
 @push('page-level-scripts')
     <script type="text/javascript" src="{{ asset('assets/pages/scripts/search.min.js') }}"></script>
 @endpush
+
+@push('page-level-scripts')
+    <script>
+        $('body').addClass('page-container-bg-solid');
+    </script>
+@endpush
