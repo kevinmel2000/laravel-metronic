2619093d5e954702728d5944cd5d28000baac6d3
R. Kukuh <rkukuh@gmail.com>
2017-07-16 05:46:03 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/general/pricing-1.blade.php
+++ resources/views/admin-1/pages/general/pricing-1.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Pricing #1')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>General</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
