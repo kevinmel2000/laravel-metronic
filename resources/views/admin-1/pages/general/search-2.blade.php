92c5196676b1a3234d21f2bc27b1074cdfba8b32
R. Kukuh <rkukuh@gmail.com>
2017-07-16 02:11:59 +0700

    Add raw content

--- resources/views/admin-1/pages/general/search-2.blade.php
+++ resources/views/admin-1/pages/general/search-2.blade.php
@@ -22,4 +22,21 @@
     <h1 class="page-title"> Search Results 2
         <small>search results</small>
     </h1>
+
+    <div class="search-page search-content-2">
+        <div class="search-bar ">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn blue uppercase bold" type="button">Search</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <p class="search-desc clearfix"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur pellentesque auctor. Morbi lobortis, leo in tristique scelerisque. </p>
+                </div>
+            </div>
+        </div>
 @endsection
