c4cce6f278967f7dfa423bc96022c2cd7a250662
R. Kukuh <rkukuh@gmail.com>
2017-07-16 02:27:01 +0700

    Use asset() helper to pull local assets

--- resources/views/admin-1/pages/general/search-2.blade.php
+++ resources/views/admin-1/pages/general/search-2.blade.php
@@ -314,6 +314,6 @@
 @endsection
 
 @push('page-level-plugins-header')
-    <link href="../assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet" type="text/css" />
-    <link href="../assets/global/plugins/fancybox/source/jquery.fancybox.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/fancybox/source/jquery.fancybox.css') }}">
 @endpush
