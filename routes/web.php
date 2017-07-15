9412e21f19273f5e47a4fde0d3ce3c988b9d8539
R. Kukuh <rkukuh@gmail.com>
2017-07-15 19:17:41 +0700

    Define named route

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,10 @@ Route::group(['prefix' => 'admin-1'], function () {
         return view('admin-1.pages.general._ajax.load-more3');
     })->name('admin-1.ajax.load-more3');
 
+    Route::get('_ajax/load-more4', function () {
+        return view('admin-1.pages.general._ajax.load-more4');
+    })->name('admin-1.ajax.load-more4');
+
     Route::get('_ajax/project1', function () {
         return view('admin-1.pages.general._ajax.project1');
     })->name('admin-1.ajax.project1');
