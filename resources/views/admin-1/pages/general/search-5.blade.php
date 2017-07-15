3bab8b8ea017cf5b6c1adecddde4845035a1783a
R. Kukuh <rkukuh@gmail.com>
2017-07-16 04:59:46 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/general/search-5.blade.php
+++ resources/views/admin-1/pages/general/search-5.blade.php
@@ -3,6 +3,19 @@
 @section('title', 'Search #5')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <a href="#">General</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Search</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
