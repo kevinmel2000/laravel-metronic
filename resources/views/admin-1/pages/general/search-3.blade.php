7723a457f035fb1a30f374babc26ed28f5670205
R. Kukuh <rkukuh@gmail.com>
2017-07-16 03:36:18 +0700

    Push external css files

--- resources/views/admin-1/pages/general/search-3.blade.php
+++ resources/views/admin-1/pages/general/search-3.blade.php
@@ -152,3 +152,8 @@
         </div>
     </div>
 @endsection
+
+@push('page-level-plugins-header')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/fancybox/source/jquery.fancybox.css') }}">
+@endpush
