fe9d1a36bea83258af9cbc726707caf507577e2c
R. Kukuh <rkukuh@gmail.com>
2017-07-17 16:22:22 +0700

    Use asset() helper to pull local asset

--- resources/views/admin-1/pages/general/invoice-1.blade.php
+++ resources/views/admin-1/pages/general/invoice-1.blade.php
@@ -22,7 +22,7 @@
     <div class="invoice">
         <div class="row invoice-logo">
             <div class="col-xs-6 invoice-logo-space">
-                <img src="../assets/pages/media/invoice/walmart.png" class="img-responsive" alt="">
+                <img src="{{ asset('assets/pages/media/invoice/walmart.png') }}" class="img-responsive" alt="">
             </div>
             <div class="col-xs-6">
                 <p> #5652256 / 28 Feb 2013
