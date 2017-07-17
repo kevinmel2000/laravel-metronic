100a6ae56796413f3ddcbd0b7e3653e9e3fbd873
R. Kukuh <rkukuh@gmail.com>
2017-07-17 18:34:01 +0700

    Syntax styling

--- resources/views/admin-1/pages/user/profile-1.blade.php
+++ resources/views/admin-1/pages/user/profile-1.blade.php
@@ -23,44 +23,53 @@
         <div class="col-md-12">
             <!-- BEGIN PROFILE SIDEBAR -->
             <div class="profile-sidebar">
+
                 <!-- PORTLET MAIN -->
                 <div class="portlet light profile-sidebar-portlet ">
                     <!-- SIDEBAR USERPIC -->
                     <div class="profile-userpic">
-                        <img src="assets/pages/media/profile/profile_user.jpg" class="img-responsive" alt=""> </div>
+                        <img src="assets/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
+                    </div>
                     <!-- END SIDEBAR USERPIC -->
+
                     <!-- SIDEBAR USER TITLE -->
                     <div class="profile-usertitle">
                         <div class="profile-usertitle-name"> Marcus Doe </div>
                         <div class="profile-usertitle-job"> Developer </div>
                     </div>
                     <!-- END SIDEBAR USER TITLE -->
+
                     <!-- SIDEBAR BUTTONS -->
                     <div class="profile-userbuttons">
                         <button type="button" class="btn btn-circle green btn-sm">Follow</button>
                         <button type="button" class="btn btn-circle red btn-sm">Message</button>
                     </div>
                     <!-- END SIDEBAR BUTTONS -->
+
                     <!-- SIDEBAR MENU -->
                     <div class="profile-usermenu">
                         <ul class="nav">
                             <li class="active">
                                 <a href="page_user_profile_1.html">
-                                    <i class="icon-home"></i> Overview </a>
+                                    <i class="icon-home"></i> Overview
+                                </a>
                             </li>
                             <li>
                                 <a href="page_user_profile_1_account.html">
-                                    <i class="icon-settings"></i> Account Settings </a>
+                                    <i class="icon-settings"></i> Account Settings
+                                </a>
                             </li>
                             <li>
                                 <a href="page_user_profile_1_help.html">
-                                    <i class="icon-info"></i> Help </a>
+                                    <i class="icon-info"></i> Help
+                                </a>
                             </li>
                         </ul>
                     </div>
                     <!-- END MENU -->
                 </div>
                 <!-- END PORTLET MAIN -->
+
                 <!-- PORTLET MAIN -->
                 <div class="portlet light ">
                     <!-- STAT -->
