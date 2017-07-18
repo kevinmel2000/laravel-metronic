80dd0dc46c61e29dca00d7192d1e4d25fd075a39
R. Kukuh <rkukuh@gmail.com>
2017-07-18 14:55:10 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/user/profile-1-help.blade.php
+++ resources/views/admin-1/pages/user/profile-1-help.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Profile #1 Help')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>User</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
