a5e04a7bcedd77f6286fef4722f02b6fad63b0cd
R. Kukuh <rkukuh@gmail.com>
2017-07-18 19:48:34 +0700

    Set container background color

--- resources/views/admin-1/pages/apps/inbox.blade.php
+++ resources/views/admin-1/pages/apps/inbox.blade.php
@@ -161,3 +161,10 @@
 @push('page-level-scripts')
     <script type="text/javascript" src="{{ asset('assets/apps/scripts/inbox.js') }}"></script>
 @endpush
+
+@push('page-level-scripts')
+    <script>
+        $('body').addClass('page-container-bg-solid page-sidebar-closed');
+        $('ul.page-sidebar-menu').addClass('page-sidebar-menu-closed');
+    </script>
+@endpush
