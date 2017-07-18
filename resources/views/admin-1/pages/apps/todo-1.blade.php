e54b7b4e5a9f6253d0222cebcd06c2756acd151b
R. Kukuh <rkukuh@gmail.com>
2017-07-18 17:19:07 +0700

    Add modal

--- resources/views/admin-1/pages/apps/todo-1.blade.php
+++ resources/views/admin-1/pages/apps/todo-1.blade.php
@@ -198,4 +198,162 @@
             </div>
         </div>
     </div>
+
+    <div id="todo-task-modal" class="modal fade" role="dialog" aria-labelledby="myModalLabel10" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content scroller" style="height: 100%;" data-always-visible="1" data-rail-visible="0">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <button class="btn btn-square btn-sm green todo-bold todo-inline">Complete Task</button>
+                    <p class="todo-task-modal-title todo-inline">Due:
+                        <input class="form-control input-inline input-medium date-picker todo-task-due todo-inline" size="16" type="text" value="10/01/2015" />
+                    </p>
+                    <p class="todo-task-modal-title todo-inline">Assign:
+                        <a class="todo-inline todo-task-assign" href="#todo-members-modal" data-toggle="modal">Luke</a>
+                    </p>
+                </div>
+                <div class="modal-body todo-task-modal-body">
+                    <h3 class="todo-task-modal-bg">Sample Images for Mega Menu Components Category</h3>
+                    <p class="todo-task-modal-bg"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                        suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                        <br>
+                        <br> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                        suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                        <br>
+                        <br> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                        suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
+                    <h4>Attach File
+                        <a class="todo-add-button" href="javascrip:;">+</a>
+                    </h4>
+                    <p class="todo-task-file">
+                        <i class="fa fa-file-o todo-grey"></i> menu_screen.jpg
+                        <i class="fa fa-times todo-remove-file"></i>
+                    </p>
+                    <p class="todo-task-file">
+                        <i class="fa fa-file-o todo-grey"></i> comments.jpg
+                        <i class="fa fa-times todo-remove-file"></i>
+                    </p>
+                </div>
+                <!-- BEGIN PORTLET-->
+                <div class="portlet light ">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="icon-bubble font-hide hide"></i>
+                            <span class="caption-subject font-hide bold uppercase">Chats</span>
+                        </div>
+                        <div class="actions">
+                            <div class="portlet-input input-inline">
+                                <div class="input-icon right">
+                                    <i class="icon-magnifier"></i>
+                                    <input type="text" class="form-control input-circle" placeholder="search..."> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="portlet-body" id="chats">
+                        <div class="scroller" style="height: 525px;" data-always-visible="1" data-rail-visible1="1">
+                            <ul class="chats">
+                                <li class="out">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar2.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Lisa Wong </a>
+                                        <span class="datetime"> at 20:11 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="out">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar2.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Lisa Wong </a>
+                                        <span class="datetime"> at 20:11 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="in">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar1.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Bob Nilson </a>
+                                        <span class="datetime"> at 20:30 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="in">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar1.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Bob Nilson </a>
+                                        <span class="datetime"> at 20:30 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="out">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar3.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Richard Doe </a>
+                                        <span class="datetime"> at 20:33 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="in">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar3.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Richard Doe </a>
+                                        <span class="datetime"> at 20:35 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="out">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar1.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Bob Nilson </a>
+                                        <span class="datetime"> at 20:40 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="in">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar3.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Richard Doe </a>
+                                        <span class="datetime"> at 20:40 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </span>
+                                    </div>
+                                </li>
+                                <li class="out">
+                                    <img class="avatar" alt="" src="../assets/layouts/layout/img/avatar1.jpg" />
+                                    <div class="message">
+                                        <span class="arrow"> </span>
+                                        <a href="javascript:;" class="name"> Bob Nilson </a>
+                                        <span class="datetime"> at 20:54 </span>
+                                        <span class="body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. sed diam nonummy nibh euismod tincidunt ut laoreet.
+                                            </span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="chat-form">
+                            <div class="input-cont">
+                                <input class="form-control" type="text" placeholder="Type a message here..." /> </div>
+                            <div class="btn-cont">
+                                <span class="arrow"> </span>
+                                <a href="" class="btn blue icn-only">
+                                    <i class="fa fa-check icon-white"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END PORTLET-->
+                <div class="modal-footer">
+                    <button class="btn default" data-dismiss="modal" aria-hidden="true">Close</button>
+                    <button class="btn green" data-dismiss="modal">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
