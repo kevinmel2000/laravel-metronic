73d80e11e1ef3560a1651b755ee37309c97c40a9
R. Kukuh <rkukuh@gmail.com>
2017-07-18 17:33:37 +0700

    Change breadcrumb structure

--- resources/views/admin-1/pages/apps/todo-1.blade.php
+++ resources/views/admin-1/pages/apps/todo-1.blade.php
@@ -3,19 +3,21 @@
 @section('title', 'Todo #1')
 
 @section('breadcrumb')
-    <h3>ToDo App</h3>
+    <div class="todo-main-header">
+        <h3>ToDo App</h3>
 
-    <ul class="todo-breadcrumb">
-        <li>
-            <a href="javascrip:;">Home</a>
-        </li>
-        <li>
-            <a href="javascrip:;">Extra</a>
-        </li>
-        <li>
-            <a class="todo-active" href="javascrip:;">Todo</a>
-        </li>
-    </ul>
+        <ul class="todo-breadcrumb">
+            <li>
+                <a href="javascrip:;">Home</a>
+            </li>
+            <li>
+                <a href="javascrip:;">Extra</a>
+            </li>
+            <li>
+                <a class="todo-active" href="javascrip:;">Todo</a>
+            </li>
+        </ul>
+    </div>
 @endsection
 
 @section('content')
