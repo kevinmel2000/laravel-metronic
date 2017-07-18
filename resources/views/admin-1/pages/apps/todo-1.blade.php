c4efd22d2fa556f6c0b2e45437172333bd71afed
R. Kukuh <rkukuh@gmail.com>
2017-07-18 17:16:57 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/apps/todo-1.blade.php
+++ resources/views/admin-1/pages/apps/todo-1.blade.php
@@ -3,6 +3,17 @@
 @section('title', 'Todo #1')
 
 @section('breadcrumb')
+    <ul class="todo-breadcrumb">
+        <li>
+            <a href="javascrip:;">Home</a>
+        </li>
+        <li>
+            <a href="javascrip:;">Extra</a>
+        </li>
+        <li>
+            <a class="todo-active" href="javascrip:;">Todo</a>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
