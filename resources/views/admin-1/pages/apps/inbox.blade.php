bbf52c56aa68dbb7007025a2ce653aaee78854fc
R. Kukuh <rkukuh@gmail.com>
2017-07-18 19:16:38 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/apps/inbox.blade.php
+++ resources/views/admin-1/pages/apps/inbox.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Inbox')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Apps</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
