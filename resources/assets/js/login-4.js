f8dbe9d067378577f49370d9e073c2a866849078
R. Kukuh <rkukuh@gmail.com>
2017-07-19 14:47:04 +0700

    Fix: Incorrect file path

--- resources/assets/js/login-4.js
+++ resources/assets/js/login-4.js
@@ -255,10 +255,10 @@ var Login = function () {
 
             // init background slide images
 		    $.backstretch([
-		        "../assets/pages/media/bg/1.jpg",
-		        "../assets/pages/media/bg/2.jpg",
-		        "../assets/pages/media/bg/3.jpg",
-		        "../assets/pages/media/bg/4.jpg"
+		        "../../assets/pages/media/bg/1.jpg",
+		        "../../assets/pages/media/bg/2.jpg",
+		        "../../assets/pages/media/bg/3.jpg",
+		        "../../assets/pages/media/bg/4.jpg"
 		        ], {
 		          fade: 1000,
 		          duration: 8000
