faaf8cf86240e75e410866972dd51df53cf838bf
R. Kukuh <rkukuh@gmail.com>
2017-07-19 12:46:07 +0700

    No need to Mix laravel's default app.js asset

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.scripts(['resources/assets/js/coming-soon.js'], 'public/js/coming-soon.min.js')
    .version();
