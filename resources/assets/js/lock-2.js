284059a566bfbc492d089ef945fbc72705a17484
R. Kukuh <rkukuh@gmail.com>
2017-07-19 13:39:00 +0700

    Fix: Incorrect file path

--- resources/assets/js/lock-2.js
+++ resources/assets/js/lock-2.js
@@ -5,14 +5,14 @@ var Lock = function () {
         init: function () {
 
              $.backstretch([
-		        "../assets/pages/media/bg/1.jpg",
-    		    "../assets/pages/media/bg/2.jpg",
-    		    "../assets/pages/media/bg/3.jpg",
-    		    "../assets/pages/media/bg/4.jpg"
-		        ], {
-		          fade: 1000,
-		          duration: 8000
-		      });
+                 "../../assets/pages/media/bg/1.jpg",
+     		    "../../assets/pages/media/bg/2.jpg",
+     		    "../../assets/pages/media/bg/3.jpg",
+     		    "../../assets/pages/media/bg/4.jpg"
+             ], {
+                fade: 1000,
+                duration: 8000
+             });
         }
 
     };
