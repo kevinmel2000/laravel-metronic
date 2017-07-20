47397e94c847491ad8901892f1cb694c1975ce17
R. Kukuh <rkukuh@gmail.com>
2017-07-20 12:30:50 +0700

    Use '>' instead of '/>'

--- resources/views/admin-1/pages/apps/calendar.blade.php
+++ resources/views/admin-1/pages/apps/calendar.blade.php
@@ -35,17 +35,17 @@
                             <h3 class="event-form-title margin-bottom-20">Draggable Events</h3>
                             <div id="external-events">
                                 <form class="inline-form">
-                                    <input type="text" value="" class="form-control" placeholder="Event Title..." id="event_title" />
+                                    <input type="text" value="" class="form-control" placeholder="Event Title..." id="event_title">
                                     <br/>
                                     <a href="javascript:;" id="event_add" class="btn green"> Add Event </a>
                                 </form>
                                 <hr/>
                                 <div id="event_box" class="margin-bottom-10"></div>
                                 <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline" for="drop-remove"> remove after drop
-                                    <input type="checkbox" class="group-checkable" id="drop-remove" />
+                                    <input type="checkbox" class="group-checkable" id="drop-remove">
                                     <span></span>
                                 </label>
-                                <hr class="visible-xs" /> </div>
+                                <hr class="visible-xs"> </div>
                             <!-- END DRAGGABLE EVENTS PORTLET-->
                         </div>
                         <div class="col-md-9 col-sm-12">
