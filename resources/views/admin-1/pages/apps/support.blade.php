b309c8f95d9b20754263ce8ebf2859d14a02290c
R. Kukuh <rkukuh@gmail.com>
2017-07-20 12:47:14 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/apps/support.blade.php
+++ resources/views/admin-1/pages/apps/support.blade.php
@@ -1,3 +1,15 @@
 @extends('layouts.admin-1.master')
 
 @section('title', 'Support Ticket')
+
+@section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>Apps</span>
+        </li>
+    </ul>
+@endsection
