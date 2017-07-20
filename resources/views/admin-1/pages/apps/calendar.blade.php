dd410c9054c64a6802cd8fa79d11add7785e8d49
R. Kukuh <rkukuh@gmail.com>
2017-07-20 12:30:11 +0700

    Update content

--- resources/views/admin-1/pages/apps/calendar.blade.php
+++ resources/views/admin-1/pages/apps/calendar.blade.php
@@ -18,4 +18,42 @@
     <h1 class="page-title"> Calendar
         <small>calendar page</small>
     </h1>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="portlet light portlet-fit bordered calendar">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class=" icon-layers font-green"></i>
+                        <span class="caption-subject font-green sbold uppercase">Calendar</span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-12">
+                            <!-- BEGIN DRAGGABLE EVENTS PORTLET-->
+                            <h3 class="event-form-title margin-bottom-20">Draggable Events</h3>
+                            <div id="external-events">
+                                <form class="inline-form">
+                                    <input type="text" value="" class="form-control" placeholder="Event Title..." id="event_title" />
+                                    <br/>
+                                    <a href="javascript:;" id="event_add" class="btn green"> Add Event </a>
+                                </form>
+                                <hr/>
+                                <div id="event_box" class="margin-bottom-10"></div>
+                                <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline" for="drop-remove"> remove after drop
+                                    <input type="checkbox" class="group-checkable" id="drop-remove" />
+                                    <span></span>
+                                </label>
+                                <hr class="visible-xs" /> </div>
+                            <!-- END DRAGGABLE EVENTS PORTLET-->
+                        </div>
+                        <div class="col-md-9 col-sm-12">
+                            <div id="calendar" class="has-toolbar"> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
