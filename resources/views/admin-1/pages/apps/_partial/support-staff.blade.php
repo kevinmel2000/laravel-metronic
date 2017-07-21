28c96707eefc6670a16bb2e319508e30ac1694a5
R. Kukuh <rkukuh@gmail.com>
2017-07-21 11:36:32 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/apps/_partial/support-staff.blade.php
+++ resources/views/admin-1/pages/apps/_partial/support-staff.blade.php
@@ -1,3 +1,12 @@
 @extends('layouts.admin-1.master')
 
 @section('title', 'Ticket Support Staff Page')
+
+@section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+    </ul>
+@endsection
