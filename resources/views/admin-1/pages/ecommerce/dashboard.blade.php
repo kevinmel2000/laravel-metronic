df0b2a5326e19111b3ab3f22527a57818c6b1ca8
R. Kukuh <rkukuh@gmail.com>
2017-07-23 17:33:20 +0700

    Add dashboard stat

--- resources/views/admin-1/pages/ecommerce/dashboard.blade.php
+++ resources/views/admin-1/pages/ecommerce/dashboard.blade.php
@@ -18,4 +18,49 @@
     <h1 class="page-title"> eCommercen Dashboard
         <small>recent ecommerce statistics, charts, recent orders and sales</small>
     </h1>
+
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 margin-bottom-10">
+            <div class="dashboard-stat blue">
+                <div class="visual">
+                    <i class="fa fa-briefcase fa-icon-medium"></i>
+                </div>
+                <div class="details">
+                    <div class="number"> $168,492.54 </div>
+                    <div class="desc"> Lifetime Sales </div>
+                </div>
+                <a class="more" href="javascript:;"> View more
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div class="dashboard-stat red">
+                <div class="visual">
+                    <i class="fa fa-shopping-cart"></i>
+                </div>
+                <div class="details">
+                    <div class="number"> 1,127,390 </div>
+                    <div class="desc"> Total Orders </div>
+                </div>
+                <a class="more" href="javascript:;"> View more
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div class="dashboard-stat green">
+                <div class="visual">
+                    <i class="fa fa-group fa-icon-medium"></i>
+                </div>
+                <div class="details">
+                    <div class="number"> $670.54 </div>
+                    <div class="desc"> Average Orders </div>
+                </div>
+                <a class="more" href="javascript:;"> View more
+                    <i class="m-icon-swapright m-icon-white"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
