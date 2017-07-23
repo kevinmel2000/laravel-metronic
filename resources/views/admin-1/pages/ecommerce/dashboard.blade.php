139d36a0a98d0ad2ac76c1f1d63648763f829bb3
R. Kukuh <rkukuh@gmail.com>
2017-07-23 17:42:29 +0700

    Push external js assets

--- resources/views/admin-1/pages/ecommerce/dashboard.blade.php
+++ resources/views/admin-1/pages/ecommerce/dashboard.blade.php
@@ -1220,3 +1220,21 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/fullcalendar/fullcalendar.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/jqvmap/jqvmap/jqvmap.css') }}">
 @endpush
+
+@push('page-level-plugins-footer')
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/moment.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/fullcalendar/fullcalendar.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/flot/jquery.flot.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/flot/jquery.flot.resize.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/flot/jquery.flot.categories.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.sparkline.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js') }}"></script>
+@endpush
