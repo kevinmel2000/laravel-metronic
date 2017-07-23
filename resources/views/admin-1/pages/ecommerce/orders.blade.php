409e35db6b72ebaab6aa58d6e028f26e0a434831
R. Kukuh <rkukuh@gmail.com>
2017-07-23 17:51:36 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/ecommerce/orders.blade.php
+++ resources/views/admin-1/pages/ecommerce/orders.blade.php
@@ -1,3 +1,15 @@
 @extends('layouts.admin-1.master')
 
 @section('title', 'eCommerce Orders')
+
+@section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>eCommerce</span>
+        </li>
+    </ul>
+@endsection
