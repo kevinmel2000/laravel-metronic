a1440745d18241ea13ea3f25ce0d183a431b2ca3
R. Kukuh <rkukuh@gmail.com>
2017-07-23 17:53:14 +0700

    Add raw content

--- resources/views/admin-1/pages/ecommerce/orders.blade.php
+++ resources/views/admin-1/pages/ecommerce/orders.blade.php
@@ -18,4 +18,141 @@
     <h1 class="page-title"> eCommerce Orders
         <small>manage and track orders</small>
     </h1>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="note note-info">
+                <p> NOTE: The below datatable is not connected to a real database so the filter and sorting is just simulated for demo purposes only. </p>
+            </div>
+            <!-- Begin: life time stats -->
+            <div class="portlet light portlet-fit portlet-datatable bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-settings font-green"></i>
+                        <span class="caption-subject font-green sbold uppercase"> Order Listing </span>
+                    </div>
+                    <div class="actions">
+                        <div class="btn-group btn-group-devided" data-toggle="buttons">
+                            <label class="btn btn-transparent green btn-outline btn-outline btn-circle btn-sm active">
+                                <input type="radio" name="options" class="toggle" id="option1">Actions</label>
+                            <label class="btn btn-transparent blue btn-outline btn-circle btn-sm">
+                                <input type="radio" name="options" class="toggle" id="option2">Settings</label>
+                        </div>
+                        <div class="btn-group">
+                            <a class="btn red btn-outline btn-circle" href="javascript:;" data-toggle="dropdown">
+                                <i class="fa fa-share"></i>
+                                <span class="hidden-xs"> Tools </span>
+                                <i class="fa fa-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu pull-right">
+                                <li>
+                                    <a href="javascript:;"> Export to Excel </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;"> Export to CSV </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;"> Export to XML </a>
+                                </li>
+                                <li class="divider"> </li>
+                                <li>
+                                    <a href="javascript:;"> Print Invoices </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div class="table-container">
+                        <div class="table-actions-wrapper">
+                            <span> </span>
+                            <select class="table-group-action-input form-control input-inline input-small input-sm">
+                                <option value="">Select...</option>
+                                <option value="Cancel">Cancel</option>
+                                <option value="Cancel">Hold</option>
+                                <option value="Cancel">On Hold</option>
+                                <option value="Close">Close</option>
+                            </select>
+                            <button class="btn btn-sm btn-default table-group-action-submit">
+                                <i class="fa fa-check"></i> Submit</button>
+                        </div>
+                        <table class="table table-striped table-bordered table-hover table-checkable" id="datatable_orders">
+                            <thead>
+                                <tr role="row" class="heading">
+                                    <th width="2%">
+                                        <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                            <input type="checkbox" class="group-checkable" data-set="#sample_2 .checkboxes" />
+                                            <span></span>
+                                        </label>
+                                    </th>
+                                    <th width="5%"> Order&nbsp;# </th>
+                                    <th width="15%"> Purchased&nbsp;On </th>
+                                    <th width="15%"> Customer </th>
+                                    <th width="10%"> Ship&nbsp;To </th>
+                                    <th width="10%"> Base&nbsp;Price </th>
+                                    <th width="10%"> Purchased&nbsp;Price </th>
+                                    <th width="10%"> Status </th>
+                                    <th width="10%"> Actions </th>
+                                </tr>
+                                <tr role="row" class="filter">
+                                    <td> </td>
+                                    <td>
+                                        <input type="text" class="form-control form-filter input-sm" name="order_id"> </td>
+                                    <td>
+                                        <div class="input-group date date-picker margin-bottom-5" data-date-format="dd/mm/yyyy">
+                                            <input type="text" class="form-control form-filter input-sm" readonly name="order_date_from" placeholder="From">
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-sm default" type="button">
+                                                    <i class="fa fa-calendar"></i>
+                                                </button>
+                                            </span>
+                                        </div>
+                                        <div class="input-group date date-picker" data-date-format="dd/mm/yyyy">
+                                            <input type="text" class="form-control form-filter input-sm" readonly name="order_date_to" placeholder="To">
+                                            <span class="input-group-btn">
+                                                <button class="btn btn-sm default" type="button">
+                                                    <i class="fa fa-calendar"></i>
+                                                </button>
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control form-filter input-sm" name="order_customer_name"> </td>
+                                    <td>
+                                        <input type="text" class="form-control form-filter input-sm" name="order_ship_to"> </td>
+                                    <td>
+                                        <div class="margin-bottom-5">
+                                            <input type="text" class="form-control form-filter input-sm" name="order_base_price_from" placeholder="From" /> </div>
+                                        <input type="text" class="form-control form-filter input-sm" name="order_base_price_to" placeholder="To" /> </td>
+                                    <td>
+                                        <div class="margin-bottom-5">
+                                            <input type="text" class="form-control form-filter input-sm margin-bottom-5 clearfix" name="order_purchase_price_from" placeholder="From" /> </div>
+                                        <input type="text" class="form-control form-filter input-sm" name="order_purchase_price_to" placeholder="To" /> </td>
+                                    <td>
+                                        <select name="order_status" class="form-control form-filter input-sm">
+                                            <option value="">Select...</option>
+                                            <option value="pending">Pending</option>
+                                            <option value="closed">Closed</option>
+                                            <option value="hold">On Hold</option>
+                                            <option value="fraud">Fraud</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <div class="margin-bottom-5">
+                                            <button class="btn btn-sm btn-success filter-submit margin-bottom">
+                                                <i class="fa fa-search"></i> Search</button>
+                                        </div>
+                                        <button class="btn btn-sm btn-default filter-cancel">
+                                            <i class="fa fa-times"></i> Reset</button>
+                                    </td>
+                                </tr>
+                            </thead>
+                            <tbody> </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- End: life time stats -->
+        </div>
+    </div>
 @endsection
