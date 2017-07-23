f90dcd9fe03a671b65178a3b1f617dfa72e8c3a1
R. Kukuh <rkukuh@gmail.com>
2017-07-23 17:47:37 +0700

    Collapse sidebar by default

--- resources/views/admin-1/pages/ecommerce/dashboard.blade.php
+++ resources/views/admin-1/pages/ecommerce/dashboard.blade.php
@@ -1242,3 +1242,10 @@
 @push('page-level-scripts')
     <script type="text/javascript" src="{{ asset('assets/pages/scripts/ecommerce-dashboard.min.js') }}"></script>
 @endpush
+
+@push('page-level-scripts')
+    <script>
+        $('body').addClass('page-sidebar-closed');
+        $('ul.page-sidebar-menu').addClass('page-sidebar-menu-closed');
+    </script>
+@endpush
