e84f008dc6b2802ea3c3b610ee16b8f1b408e70c
R. Kukuh <rkukuh@gmail.com>
2017-08-04 16:00:27 +0700

    Use blank ('#') as anchor link

--- resources/views/layouts/admin-1/sidebar/dashboard.blade.php
+++ resources/views/layouts/admin-1/sidebar/dashboard.blade.php
@@ -6,20 +6,20 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item start ">
-            <a href="{{ route('admin-1.ecommerce-dashboard') }}" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <i class="icon-bar-chart"></i>
                 <span class="title">Dashboard 1</span>
             </a>
         </li>
         <li class="nav-item start ">
-            <a href="dashboard_2.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <i class="icon-bulb"></i>
                 <span class="title">Dashboard 2</span>
                 <span class="badge badge-success">1</span>
             </a>
         </li>
         <li class="nav-item start ">
-            <a href="dashboard_3.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <i class="icon-graph"></i>
                 <span class="title">Dashboard 3</span>
                 <span class="badge badge-danger">5</span>
