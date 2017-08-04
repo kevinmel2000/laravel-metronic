bd8389ace01f3c6cc1dbb5c01eb5873f150c2d67
R. Kukuh <rkukuh@gmail.com>
2017-08-04 15:57:26 +0700

    Open sidebar instead of collapsed

--- resources/views/admin-1/pages/ecommerce/dashboard.blade.php
+++ resources/views/admin-1/pages/ecommerce/dashboard.blade.php
@@ -1242,10 +1242,3 @@
 @push('page-level-scripts')
     <script type="text/javascript" src="{{ asset('assets/pages/scripts/ecommerce-dashboard.min.js') }}"></script>
 @endpush
-
-@push('page-level-scripts')
-    <script>
-        $('body').addClass('page-sidebar-closed');
-        $('ul.page-sidebar-menu').addClass('page-sidebar-menu-closed');
-    </script>
-@endpush
