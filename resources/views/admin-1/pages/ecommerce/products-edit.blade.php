9895cc0131994eaa25299456a75a2c021af89377
R. Kukuh <rkukuh@gmail.com>
2017-08-04 10:08:37 +0700

    Add breadcrumb

--- resources/views/admin-1/pages/ecommerce/products-edit.blade.php
+++ resources/views/admin-1/pages/ecommerce/products-edit.blade.php
@@ -1,3 +1,15 @@
 @extends('layouts.admin-1.master')
 
 @section('title', 'eCommerce Products Edit')
+
+@section('breadcrum')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>eCommerce</span>
+        </li>
+    </ul>
+@endsection
