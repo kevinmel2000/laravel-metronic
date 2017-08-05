8840d5a8533202370fdd90f1b13914f80eb34b5d
R. Kukuh <rkukuh@gmail.com>
2017-08-05 16:35:58 +0700

    Set blank to all unrouted links

--- resources/views/layouts/admin-1/sidebar/portlet.blade.php
+++ resources/views/layouts/admin-1/sidebar/portlet.blade.php
@@ -6,27 +6,27 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item  ">
-            <a href="portlet_boxed.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Boxed Portlets</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="portlet_light.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Light Portlets</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="portlet_solid.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Solid Portlets</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="portlet_ajax.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Ajax Portlets</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="portlet_draggable.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Draggable Portlets</span>
             </a>
         </li>
