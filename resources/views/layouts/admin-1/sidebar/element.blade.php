507c90393a942ed260c2c4364950b0770a45453f
R. Kukuh <rkukuh@gmail.com>
2017-08-05 16:26:32 +0700

    Set blank to all unrouted links

--- resources/views/layouts/admin-1/sidebar/element.blade.php
+++ resources/views/layouts/admin-1/sidebar/element.blade.php
@@ -6,27 +6,27 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item  ">
-            <a href="elements_steps.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Steps</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="elements_lists.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Lists</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="elements_ribbons.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Ribbons</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="elements_overlay.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Overlays</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="elements_cards.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">User Cards</span>
             </a>
         </li>
