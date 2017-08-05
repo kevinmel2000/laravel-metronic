841ef14c8f1a066df712a434892db0f75b3a8394
R. Kukuh <rkukuh@gmail.com>
2017-08-05 16:36:38 +0700

    Set blank to all unrouted links

--- resources/views/layouts/admin-1/sidebar/table.blade.php
+++ resources/views/layouts/admin-1/sidebar/table.blade.php
@@ -6,17 +6,17 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item  ">
-            <a href="table_static_basic.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Basic Tables</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="table_static_responsive.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Responsive Tables</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="table_bootstrap.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Bootstrap Tables</span>
             </a>
         </li>
@@ -27,31 +27,31 @@
             </a>
             <ul class="sub-menu">
                 <li class="nav-item ">
-                    <a href="table_datatables_managed.html" class="nav-link "> Managed Datatables </a>
+                    <a href="#" class="nav-link "> Managed Datatables </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_buttons.html" class="nav-link "> Buttons Extension </a>
+                    <a href="#" class="nav-link "> Buttons Extension </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_colreorder.html" class="nav-link "> Colreorder Extension </a>
+                    <a href="#" class="nav-link "> Colreorder Extension </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_rowreorder.html" class="nav-link "> Rowreorder Extension </a>
+                    <a href="#" class="nav-link "> Rowreorder Extension </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_scroller.html" class="nav-link "> Scroller Extension </a>
+                    <a href="#" class="nav-link "> Scroller Extension </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_fixedheader.html" class="nav-link "> FixedHeader Extension </a>
+                    <a href="#" class="nav-link "> FixedHeader Extension </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_responsive.html" class="nav-link "> Responsive Extension </a>
+                    <a href="#" class="nav-link "> Responsive Extension </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_editable.html" class="nav-link "> Editable Datatables </a>
+                    <a href="#" class="nav-link "> Editable Datatables </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="table_datatables_ajax.html" class="nav-link "> Ajax Datatables </a>
+                    <a href="#" class="nav-link "> Ajax Datatables </a>
                 </li>
             </ul>
         </li>
