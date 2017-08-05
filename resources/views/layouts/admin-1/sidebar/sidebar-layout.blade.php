028083eafdb3c10f5cab7ca72d249d1f270dffa5
R. Kukuh <rkukuh@gmail.com>
2017-08-05 16:36:19 +0700

    Set blank to all unrouted links

--- resources/views/layouts/admin-1/sidebar/sidebar-layout.blade.php
+++ resources/views/layouts/admin-1/sidebar/sidebar-layout.blade.php
@@ -6,42 +6,42 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item  ">
-            <a href="layout_sidebar_menu_light.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Light Sidebar Menu</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_sidebar_menu_hover.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Hover Sidebar Menu</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_sidebar_search_1.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Sidebar Search Option 1</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_sidebar_search_2.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Sidebar Search Option 2</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_toggler_on_sidebar.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Sidebar Toggler On Sidebar</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_sidebar_reversed.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Reversed Sidebar Page</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_sidebar_fixed.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Fixed Sidebar Layout</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="layout_sidebar_closed.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Closed Sidebar Layout</span>
             </a>
         </li>
