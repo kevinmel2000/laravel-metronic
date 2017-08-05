b24c7d4c98ac6a2dbd029187fec7425ca0ce289b
R. Kukuh <rkukuh@gmail.com>
2017-08-05 16:25:31 +0700

    Set blank to all unrouted links

--- resources/views/layouts/admin-1/sidebar/chart.blade.php
+++ resources/views/layouts/admin-1/sidebar/chart.blade.php
@@ -6,32 +6,32 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item  ">
-            <a href="charts_amcharts.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">amChart</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="charts_flotcharts.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Flot Charts</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="charts_flowchart.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Flow Charts</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="charts_google.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Google Charts</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="charts_echarts.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">eCharts</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="charts_morris.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Morris Charts</span>
             </a>
         </li>
@@ -42,13 +42,13 @@
             </a>
             <ul class="sub-menu">
                 <li class="nav-item ">
-                    <a href="charts_highcharts.html" class="nav-link "> HighCharts </a>
+                    <a href="#" class="nav-link "> HighCharts </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="charts_highstock.html" class="nav-link "> HighStock </a>
+                    <a href="#" class="nav-link "> HighStock </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="charts_highmaps.html" class="nav-link "> HighMaps </a>
+                    <a href="#" class="nav-link "> HighMaps </a>
                 </li>
             </ul>
         </li>
