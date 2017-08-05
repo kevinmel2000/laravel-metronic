7343fbf6a12922d733bbd7880d1f05131d3eaffd
R. Kukuh <rkukuh@gmail.com>
2017-08-05 16:30:57 +0700

    Set blank to all unrouted links

--- resources/views/layouts/admin-1/sidebar/form-stuff.blade.php
+++ resources/views/layouts/admin-1/sidebar/form-stuff.blade.php
@@ -6,80 +6,80 @@
     </a>
     <ul class="sub-menu">
         <li class="nav-item  ">
-            <a href="form_controls.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Bootstrap Form
                     <br>Controls
                 </span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_controls_md.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Material Design
                     <br>Form Controls
                 </span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_validation.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Form Validation</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_validation_states_md.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Material Design
                     <br>Form Validation States
                 </span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_validation_md.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Material Design
                     <br>Form Validation
                 </span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_layouts.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Form Layouts</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_repeater.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Form Repeater</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_input_mask.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Form Input Mask</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_editable.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Form X-editable</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_wizard.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Form Wizard</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_icheck.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">iCheck Controls</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_image_crop.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Image Cropping</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_fileupload.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Multiple File Upload</span>
             </a>
         </li>
         <li class="nav-item  ">
-            <a href="form_dropzone.html" class="nav-link ">
+            <a href="#" class="nav-link ">
                 <span class="title">Dropzone File Upload</span>
             </a>
         </li>
