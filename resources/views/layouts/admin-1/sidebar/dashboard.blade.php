8338f65f42d7870edfb8aada5c3005f53c7fd9fa
R. Kukuh <rkukuh@gmail.com>
2017-08-06 20:57:11 +0700

    Use route() instead

--- resources/views/layouts/admin-1/sidebar/dashboard.blade.php
+++ resources/views/layouts/admin-1/sidebar/dashboard.blade.php
@@ -12,7 +12,7 @@
             </a>
         </li>
         <li class="nav-item start ">
-            <a href="#" class="nav-link ">
+            <a href="{{ route('admin-1.dashboard.02') }}" class="nav-link ">
                 <i class="icon-bulb"></i>
                 <span class="title">Dashboard 2</span>
                 <span class="badge badge-success">1</span>
