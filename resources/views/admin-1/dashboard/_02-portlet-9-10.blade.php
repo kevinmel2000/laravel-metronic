afe60e7908ac069ddb8b63225757125f81467fc1
R. Kukuh <rkukuh@gmail.com>
2017-08-08 23:30:42 +0700

    Create and initialize partial view

---
+++ resources/views/admin-1/dashboard/_02-portlet-9-10.blade.php
@@ -0,0 +1,96 @@
+<div class="row">
+    <div class="col-lg-6 col-xs-12 col-sm-12">
+        <div class="portlet light ">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="icon-cursor font-dark hide"></i>
+                    <span class="caption-subject font-dark bold uppercase">General Stats</span>
+                </div>
+                <div class="actions">
+                    <a href="javascript:;" class="btn btn-sm btn-circle red easy-pie-chart-reload">
+                        <i class="fa fa-repeat"></i> Reload </a>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="easy-pie-chart">
+                            <div class="number transactions" data-percent="55">
+                                <span>+55</span>% </div>
+                            <a class="title" href="javascript:;"> Transactions
+                                <i class="icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="margin-bottom-10 visible-sm"> </div>
+                    <div class="col-md-4">
+                        <div class="easy-pie-chart">
+                            <div class="number visits" data-percent="85">
+                                <span>+85</span>% </div>
+                            <a class="title" href="javascript:;"> New Visits
+                                <i class="icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="margin-bottom-10 visible-sm"> </div>
+                    <div class="col-md-4">
+                        <div class="easy-pie-chart">
+                            <div class="number bounce" data-percent="46">
+                                <span>-46</span>% </div>
+                            <a class="title" href="javascript:;"> Bounce
+                                <i class="icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-xs-12 col-sm-12">
+        <div class="portlet light ">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="icon-equalizer font-dark hide"></i>
+                    <span class="caption-subject font-dark bold uppercase">Server Stats</span>
+                    <span class="caption-helper">monthly stats...</span>
+                </div>
+                <div class="tools">
+                    <a href="" class="collapse"> </a>
+                    <a href="#portlet-config" data-toggle="modal" class="config"> </a>
+                    <a href="" class="reload"> </a>
+                    <a href="" class="remove"> </a>
+                </div>
+            </div>
+            <div class="portlet-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="sparkline-chart">
+                            <div class="number" id="sparkline_bar5"></div>
+                            <a class="title" href="javascript:;"> Network
+                                <i class="icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="margin-bottom-10 visible-sm"> </div>
+                    <div class="col-md-4">
+                        <div class="sparkline-chart">
+                            <div class="number" id="sparkline_bar6"></div>
+                            <a class="title" href="javascript:;"> CPU Load
+                                <i class="icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="margin-bottom-10 visible-sm"> </div>
+                    <div class="col-md-4">
+                        <div class="sparkline-chart">
+                            <div class="number" id="sparkline_line"></div>
+                            <a class="title" href="javascript:;"> Load Rate
+                                <i class="icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
