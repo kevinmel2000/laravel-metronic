302f4507870a669271e06854f2fb39dba5c49e99
R. Kukuh <rkukuh@gmail.com>
2017-08-09 09:46:07 +0700

    Add breadcrumb

--- resources/views/admin-1/dashboard/03.blade.php
+++ resources/views/admin-1/dashboard/03.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Admin Dashboard 3')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-angle-right"></i>
+        </li>
+        <li>
+            <span>Dashboard</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
