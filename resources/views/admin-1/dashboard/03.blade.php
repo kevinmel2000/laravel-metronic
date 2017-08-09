d582a2d5a6f194f78c74ee6110c48d8063e246dc
R. Kukuh <rkukuh@gmail.com>
2017-08-09 09:47:17 +0700

    Add additional page toolbar

--- resources/views/admin-1/dashboard/03.blade.php
+++ resources/views/admin-1/dashboard/03.blade.php
@@ -12,6 +12,14 @@
             <span>Dashboard</span>
         </li>
     </ul>
+
+    <div class="page-toolbar">
+        <div id="dashboard-report-range" class="pull-right tooltips btn btn-fit-height green" data-placement="top" data-original-title="Change dashboard date range">
+            <i class="icon-calendar"></i>&nbsp;
+            <span class="thin uppercase hidden-xs"></span>&nbsp;
+            <i class="fa fa-angle-down"></i>
+        </div>
+    </div>
 @endsection
 
 @section('content')
