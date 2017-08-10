fbf6c2ea9377bd2d9f220b63ee6f2e0f414af223
R. Kukuh <rkukuh@gmail.com>
2017-08-10 11:23:50 +0700

    Push local js assets

--- resources/views/admin-1/dashboard/03.blade.php
+++ resources/views/admin-1/dashboard/03.blade.php
@@ -52,3 +52,36 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/fullcalendar/fullcalendar.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/jqvmap/jqvmap/jqvmap.css') }}">
 @endpush
+
+@push('page-level-plugins-footer')
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/moment.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/morris/morris.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/morris/raphael-min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/counterup/jquery.waypoints.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/counterup/jquery.counterup.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/amcharts.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/serial.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/pie.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/radar.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/themes/light.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/themes/patterns.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amcharts/themes/chalk.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/ammap/ammap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/ammap/maps/js/worldLow.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/amcharts/amstockcharts/amstock.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/fullcalendar/fullcalendar.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/horizontal-timeline/horizontal-timeline.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/flot/jquery.flot.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/flot/jquery.flot.resize.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/flot/jquery.flot.categories.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.sparkline.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js') }}"></script>
+@endpush
