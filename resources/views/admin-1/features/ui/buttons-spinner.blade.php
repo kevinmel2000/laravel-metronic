e9dc6cc34871d119437473b007491bd8a4dc72e0
R. Kukuh <rkukuh@gmail.com>
2017-08-11 22:11:20 +0700

    Pull local js assets

--- resources/views/admin-1/features/ui/buttons-spinner.blade.php
+++ resources/views/admin-1/features/ui/buttons-spinner.blade.php
@@ -11,3 +11,8 @@
 @push('page-level-plugins-header')
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/global/plugins/ladda/ladda-themeless.min.css') }}">
 @endpush
+
+@push('page-level-plugins-footer')
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/ladda/spin.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/ladda/ladda.min.js') }}"></script>
+@endpush
