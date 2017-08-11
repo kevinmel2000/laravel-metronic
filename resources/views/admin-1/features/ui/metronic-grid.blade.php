2f1545d47aa9506889cacde7e84c2d774365cefc
R. Kukuh <rkukuh@gmail.com>
2017-08-11 18:39:52 +0700

    Syntax styling

--- resources/views/admin-1/features/ui/metronic-grid.blade.php
+++ resources/views/admin-1/features/ui/metronic-grid.blade.php
@@ -215,57 +215,74 @@
                         </div>
                     </div>
                     <!--end: Demo 4 -->
-                    
-                            <!-- begin: Demo 5 -->
-                            <br>
-                            <h3 class="">Responsive Table Grid With Flex Cell Ordering</h3>
-                            <p> Use <code>m-grid-flex</code> and <code>m-grid-responsive-xs</code>, <code>m-grid-responsive-sm</code>, <code>m-grid-responsive-md</code> or <code>m-grid-responsive-lg</code> classes to stack the columns
-                                to vertical position for respective device widths(*-xs
-                                < 480px, *-sm < 768px, *-md < 992px, *-lg < 1260px) with defined cell flex ordering using <code>m-grid-col-order-[order_number]</code> where [order_number] can be within <code>1-12</code>. Try to resize your browser to see how it works. </p>
-                            <hr>
-                            <div class="m-grid m-grid-flex m-grid-responsive-md m-grid-demo">
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-4 bg-yellow font-white">Flex order 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-3 bg-purple font-white">Flex order 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-2 bg-green font-white">Flex order 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-1 bg-red font-white">Flex order 1</div>
-                                </div>
-                            </div>
-                            <!--end: Demo 5 -->
-                            <!-- begin: Demo 5 -->
-                            <br>
-                            <h3 class="">Full Height Inner Content</h3>
-                            <p> Use <code>m-grid m-grid-full-height</code> class to make grids full height. Also you can use nested grids to control vertical position of the content as shown below: </p>
-                            <hr>
-                            <div class="m-grid m-grid-responsive-md m-grid-demo">
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
-                                        <div class="m-grid m-grid-full-height">
-                                            <div class="m-grid-col m-grid-col-middle m-grid-col-center bg-red font-white" style="padding: 20px;">Sample cell with padding
-                                                <br>and full height content</div>
-                                        </div>
-                                    </div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
-                                        <div class="m-grid m-grid-full-height">
-                                            <div class="m-grid-col m-grid-col-top m-grid-col-right bg-green font-white" style="padding: 20px;">Sample cell with padding
-                                                <br>and full height content</div>
-                                        </div>
-                                    </div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
-                                        <div class="m-grid m-grid-full-height">
-                                            <div class="m-grid-col m-grid-col-bottom m-grid-col-left bg-yellow font-white" style="padding: 20px;">Sample cell with padding
-                                                <br>and full height content</div>
-                                        </div>
-                                    </div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
-                                        <div class="m-grid m-grid-full-height">
-                                            <div class="m-grid-col m-grid-col-middle m-grid-col-right bg-blue font-white" style="padding: 20px;">Sample cell with padding
-                                                <br>and full height content</div>
-                                        </div>
+
+                    <!-- begin: Demo 5 -->
+                    <br>
+
+                    <h3 class="">Responsive Table Grid With Flex Cell Ordering</h3>
+
+                    <p> Use <code>m-grid-flex</code> and <code>m-grid-responsive-xs</code>, <code>m-grid-responsive-sm</code>, <code>m-grid-responsive-md</code> or <code>m-grid-responsive-lg</code> classes to stack the columns
+                        to vertical position for respective device widths(*-xs
+                        < 480px, *-sm < 768px, *-md < 992px, *-lg < 1260px) with defined cell flex ordering using <code>m-grid-col-order-[order_number]</code> where [order_number] can be within <code>1-12</code>. Try to resize your browser to see how it works. </p>
+
+                    <hr>
+
+                    <div class="m-grid m-grid-flex m-grid-responsive-md m-grid-demo">
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-4 bg-yellow font-white">Flex order 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-3 bg-purple font-white">Flex order 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-2 bg-green font-white">Flex order 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-order-1 bg-red font-white">Flex order 1</div>
+                        </div>
+                    </div>
+                    <!--end: Demo 5 -->
+
+                    <!-- begin: Demo 5 -->
+                    <br>
+
+                    <h3 class="">Full Height Inner Content</h3>
+
+                    <p> Use <code>m-grid m-grid-full-height</code> class to make grids full height. Also you can use nested grids to control vertical position of the content as shown below: </p>
+
+                    <hr>
+
+                    <div class="m-grid m-grid-responsive-md m-grid-demo">
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
+                                <div class="m-grid m-grid-full-height">
+                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center bg-red font-white" style="padding: 20px;">
+                                        Sample cell with padding
+                                        <br>and full height content
                                     </div>
                                 </div>
                             </div>
-                            <!--end: Demo 5 -->
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
+                                <div class="m-grid m-grid-full-height">
+                                    <div class="m-grid-col m-grid-col-top m-grid-col-right bg-green font-white" style="padding: 20px;">
+                                        Sample cell with padding
+                                        <br>and full height content
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
+                                <div class="m-grid m-grid-full-height">
+                                    <div class="m-grid-col m-grid-col-bottom m-grid-col-left bg-yellow font-white" style="padding: 20px;">
+                                        Sample cell with padding
+                                        <br>and full height content
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center" style="padding: 30px; height: 400px">
+                                <div class="m-grid m-grid-full-height">
+                                    <div class="m-grid-col m-grid-col-middle m-grid-col-right bg-blue font-white" style="padding: 20px;">
+                                        Sample cell with padding
+                                        <br>and full height content
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end: Demo 5 -->
                 </div>
             </div>
         </div>
