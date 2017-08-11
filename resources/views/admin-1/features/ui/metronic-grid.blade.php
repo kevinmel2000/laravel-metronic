4196b5879385571ad455dfa4b877d1d2817168d6
R. Kukuh <rkukuh@gmail.com>
2017-08-11 18:38:28 +0700

    Syntax styling

--- resources/views/admin-1/features/ui/metronic-grid.blade.php
+++ resources/views/admin-1/features/ui/metronic-grid.blade.php
@@ -30,6 +30,7 @@
                     <!-- begin: Demo 1 -->
                     <h3 class="">Adjusted Table With Equal Height Cells</h3>
                     <hr>
+
                     <div class="m-grid m-grid-demo">
                         <div class="m-grid-row">
                             <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
@@ -53,15 +54,19 @@
                         </div>
                     </div>
                     <!--end: Demo 1 -->
+
                     <!-- begin: Demo 2 -->
                     <br>
+
                     <h3 class="">Column Sizing</h3>
+
                     <p> Use <code>m-grid-col-[device_width]-[size]</code> class where <code>[device_width]</code> can be <code>xs</code>
                         < 480px, <code>sm</code>
                             < 768px, <code>md</code>
                                 < 992px, <code>lg</code>
                                     < 1260px and <code>[size]</code> can be within <code>1-12</code> grid size. </p>
                     <hr>
+
                     <div class="m-grid m-grid-demo">
                         <div class="m-grid-row">
                             <div class="m-grid-col m-grid-col-middle m-grid-col-center m-grid-col-md-1">m-grid-col-md-1</div>
@@ -86,12 +91,17 @@
                         </div>
                     </div>
                     <!--end: Demo 2 -->
+
                     <!-- begin: Demo 3 -->
                     <br>
+
                     <h3 class="">Column Alignment</h3>
+
                     <p> Use <code>m-grid-col-top</code>, <code>m-grid-col-middle</code> and <code>m-grid-col-bottom</code> classes to vertically align the cell content. Use <code>m-grid-col-left</code>, <code>m-grid-col-center</code> and
                         <code>m-grid-col-right</code> classes to horizontally align the cell content. </p>
+
                     <hr>
+
                     <div class="m-grid m-grid-demo">
                         <div class="m-grid-row">
                             <div class="m-grid-col m-grid-col-top m-grid-col-center m-grid-col-md-4">Top alignment content</div>
@@ -105,93 +115,107 @@
                         </div>
                     </div>
                     <!--end: Demo 3 -->
+
                     <!-- begin: Demo 4 -->
                     <br>
+
                     <h3 class="">Responsive Table Grid</h3>
+
                     <p> Use <code>m-grid-responsive-xs</code>, <code>m-grid-responsive-sm</code>, <code>m-grid-responsive-md</code> or <code>m-grid-responsive-lg</code> classes to stack the columns to vertical position for respective device
                         widths(*-xs
                         < 480px, *-sm < 768px, *-md < 992px, *-lg < 1260px). Try to resize your browser to see how it works. </p>
-                            <hr>
-                            <h4 class="">Responsive Table Grid with <code>m-grid-responsive-xs</code></h4>
-                            <div class="m-grid m-grid-responsive-xs m-grid-demo">
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
-                                </div>
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
-                                </div>
-                            </div>
-                            <br>
-                            <h4 class="">Responsive Table Grid with <code>m-grid-responsive-sm</code></h4>
-                            <div class="m-grid m-grid-responsive-sm m-grid-demo">
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
-                                </div>
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
-                                </div>
-                            </div>
-                            <br>
-                            <h4 class="">Responsive Table Grid with <code>m-grid-responsive-md</code></h4>
-                            <div class="m-grid m-grid-responsive-md m-grid-demo">
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
-                                </div>
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
-                                </div>
-                            </div>
-                            <br>
-                            <h4 class="">Responsive Table Grid with <code>m-grid-responsive-lg</code></h4>
-                            <div class="m-grid m-grid-responsive-lg m-grid-demo">
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
-                                </div>
-                                <div class="m-grid-row">
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
-                                    <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
-                                </div>
-                            </div>
-                            <!--end: Demo 4 -->
+
+                    <hr>
+
+                    <h4 class="">Responsive Table Grid with <code>m-grid-responsive-xs</code></h4>
+                    <div class="m-grid m-grid-responsive-xs m-grid-demo">
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
+                        </div>
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <h4 class="">Responsive Table Grid with <code>m-grid-responsive-sm</code></h4>
+                    <div class="m-grid m-grid-responsive-sm m-grid-demo">
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
+                        </div>
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <h4 class="">Responsive Table Grid with <code>m-grid-responsive-md</code></h4>
+
+                    <div class="m-grid m-grid-responsive-md m-grid-demo">
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
+                        </div>
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
+                        </div>
+                    </div>
+
+                    <br>
+
+                    <h4 class="">Responsive Table Grid with <code>m-grid-responsive-lg</code></h4>
+
+                    <div class="m-grid m-grid-responsive-lg m-grid-demo">
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 1, Column 6</div>
+                        </div>
+                        <div class="m-grid-row">
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 1</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 2</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 3</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 4</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 5</div>
+                            <div class="m-grid-col m-grid-col-middle m-grid-col-center">Row 2, Column 6</div>
+                        </div>
+                    </div>
+                    <!--end: Demo 4 -->
+                    
                             <!-- begin: Demo 5 -->
                             <br>
                             <h3 class="">Responsive Table Grid With Flex Cell Ordering</h3>
