b6a43d4e1479b546ecf5b4ed9c7dd348785dd7e5
R. Kukuh <rkukuh@gmail.com>
2017-08-11 21:12:20 +0700

    Add breadcrumb

--- resources/views/admin-1/features/ui/colors.blade.php
+++ resources/views/admin-1/features/ui/colors.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Color Library')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="index.html">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>UI Features</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
