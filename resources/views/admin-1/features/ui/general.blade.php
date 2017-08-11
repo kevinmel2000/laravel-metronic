f2945c4b1a91c0e9f0d60657e8f4ff43d154517a
R. Kukuh <rkukuh@gmail.com>
2017-08-11 21:24:07 +0700

    Push local js assets

--- resources/views/admin-1/features/ui/general.blade.php
+++ resources/views/admin-1/features/ui/general.blade.php
@@ -7,3 +7,9 @@
 
 @section('content')
 @endsection
+
+@push('page-level-plugins-footer')
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery.pulsate.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/jquery-bootpag/jquery.bootpag.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/global/plugins/holder.js') }}"></script>
+@endpush
