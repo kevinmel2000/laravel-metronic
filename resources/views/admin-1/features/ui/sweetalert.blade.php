f7f1e4b8d25a60adef6115073c311a2251ceba70
R. Kukuh <rkukuh@gmail.com>
2017-08-12 15:42:04 +0700

    Add breadcrumb

--- resources/views/admin-1/features/ui/sweetalert.blade.php
+++ resources/views/admin-1/features/ui/sweetalert.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'SweetAlert for Bootstrap')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="{{ route('admin-1.index') }}">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>UI Features</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
