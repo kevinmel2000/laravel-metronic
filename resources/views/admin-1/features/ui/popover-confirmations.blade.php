353419a69aa227a4725370d3d10368251d4c9e41
R. Kukuh <rkukuh@gmail.com>
2017-08-12 15:17:03 +0700

    Add page title

--- resources/views/admin-1/features/ui/popover-confirmations.blade.php
+++ resources/views/admin-1/features/ui/popover-confirmations.blade.php
@@ -3,7 +3,19 @@
 @section('title', 'Popover Confirmations')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="{{ route('admin-1.index') }}">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>UI Features</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
+    <h1 class="page-title"> Bootstrap Confirmation
+        <small>metronic popover confirmation demos using bootstrap confirmation plugin</small>
+    </h1>
 @endsection
