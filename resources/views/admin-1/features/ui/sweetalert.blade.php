d765d03568079e362c177b105754d5e4a39f6146
R. Kukuh <rkukuh@gmail.com>
2017-08-12 15:44:44 +0700

    Add raw content

--- resources/views/admin-1/features/ui/sweetalert.blade.php
+++ resources/views/admin-1/features/ui/sweetalert.blade.php
@@ -98,4 +98,108 @@
                 data-cancel-button-text='No, I disagree' data-popup-title-success="Thank you" data-popup-message-success="You have agreed to our Terms and Conditions" data-popup-title-cancel="Cancelled" data-popup-message-cancel="You have disagreed to our Terms and Conditions">Agree to Terms & Conditions</button>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-bubble font-blue"></i>
+                        <span class="caption-subject font-blue bold uppercase">Bootstrap Sweet Alerts on Circle Buttons</span>
+                    </div>
+                    <div class="actions">
+                        <div class="btn-group">
+                            <a class="btn blue btn-outline btn-circle btn-sm" href="javascript:;" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> Actions
+                                <i class="fa fa-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu pull-right">
+                                <li>
+                                    <a href="javascript:;"> Option 1</a>
+                                </li>
+                                <li class="divider"> </li>
+                                <li>
+                                    <a href="javascript:;">Option 2</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">Option 3</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">Option 4</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <h4>Solid Circle Buttons</h4>
+                    <button class="btn btn-default btn-circle mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-default">Default Alert</button>
+                    <button class="btn btn-primary btn-circle mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-primary">Primary Alert</button>
+                    <button class="btn btn-info btn-circle mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-info">Info Alert</button>
+                    <button class="btn btn-warning btn-circle mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-warning">Warning Alert</button>
+                    <button class="btn btn-danger btn-circle mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-danger">Danger Alert</button>
+                    <button class="btn btn-success btn-circle mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-success">Success Alert</button>
+                    <h4>Outline Circle Buttons</h4>
+                    <button class="btn btn-circle green-haze btn-outline sbold uppercase mt-sweetalert" data-title="Sweet Alerts with Icons" data-message="Success Icon" data-type="success" data-allow-outside-click="true"
+                        data-confirm-button-class="btn-success">Icon Success Alert</button>
+                    <button class="btn yellow-lemon btn-circle btn-outline sbold uppercase mt-sweetalert" data-title="Sweet Alerts with Icons" data-message="Warning Icon" data-type="warning" data-allow-outside-click="true"
+                        data-confirm-button-class="btn-warning">Icon Warning Alert</button>
+                    <button class="btn red btn-circle btn-outline sbold uppercase mt-sweetalert" data-title="Sweet Alerts with Icons" data-message="Error Icon" data-type="error" data-allow-outside-click="true"
+                        data-confirm-button-class="btn-danger">Icon Error Alert</button>
+                    <button class="btn blue btn-circle btn-outline sbold uppercase mt-sweetalert" data-title="Sweet Alerts with Icons" data-message="Info Icon" data-type="info" data-allow-outside-click="true"
+                        data-confirm-button-class="btn-info">Icon Info Alert</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-bubble font-red"></i>
+                        <span class="caption-subject font-red bold uppercase">Bootstrap Sweet Alerts on Text</span>
+                    </div>
+                    <div class="actions">
+                        <div class="btn-group">
+                            <a class="btn red btn-outline btn-circle btn-sm" href="javascript:;" data-toggle="dropdown" data-hover="dropdown" data-close-others="true"> Actions
+                                <i class="fa fa-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu pull-right">
+                                <li>
+                                    <a href="javascript:;"> Option 1</a>
+                                </li>
+                                <li class="divider"> </li>
+                                <li>
+                                    <a href="javascript:;">Option 2</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">Option 3</a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">Option 4</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <h4>Standard Text Link</h4>
+                    <a href="javascript:;" class="mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-default">Standard Link</a>
+                    <a href="javascript:;" class="bold mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-primary">Bold Link</a>
+                    <a href="javascript:;" class="mt-sweetalert mt-intalic" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-info">Italic Link</a>
+                    <a href="javascript:;" class="mt-sweetalert mt-italic bold" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-warning">Mixed Text Link</a>
+                    <h4 class="mt-sweetalert-title">Colored Text Link</h4>
+                    <a href="javascript:;" class="mt-sweetalert font-red" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-danger"
+                        data-type="error">Red Text</a>
+                    <a href="javascript:;" class="bold font-green mt-sweetalert" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-primary" data-type="success">Green Bold Link</a>
+                    <a href="javascript:;" class="mt-sweetalert font-blue mt-intalic" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-info"
+                        data-type="info">Blue Italic Link</a>
+                    <a href="javascript:;" class="mt-sweetalert font-purple mt-italic bold" data-title="Sweet Alerts" data-message="Beautiful popup alerts" data-allow-outside-click="true" data-confirm-button-class="btn-warning"
+                        data-type="warning">Purple Mixed Text Link</a>
+                    <h4 class="mt-sweetalert-title">Regular Text</h4>
+                    <p class="mt-sweetalert" data-title="Sweet Alerts with Icons" data-message="Success Icon" data-type="success" data-allow-outside-click="true" data-confirm-button-class="btn-success">Icon Success Alert Pop up on regular text. Please click text.</p>
+                    <p class="mt-sweetalert" data-title="Sweet Alerts with Icons" data-message="Warning Icon" data-type="warning" data-allow-outside-click="true" data-confirm-button-class="btn-warning">It can be placed on any text paragraph. Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis,
+                        nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
