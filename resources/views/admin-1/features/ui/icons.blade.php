8cae0c2a06ae25ba6e96f866b87e16a57a3ec60d
R. Kukuh <rkukuh@gmail.com>
2017-08-12 15:58:31 +0700

    Add page title

--- resources/views/admin-1/features/ui/icons.blade.php
+++ resources/views/admin-1/features/ui/icons.blade.php
@@ -3,6 +3,15 @@
 @section('title', 'Font Icons')
 
 @section('breadcrumb')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="{{ route('admin-1.index') }}">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <span>UI Features</span>
+        </li>
+    </ul>
 @endsection
 
 @section('content')
