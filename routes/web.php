fe84ee6b393d3b692427f1b7e43adf8a10f4e75d
R. Kukuh <rkukuh@gmail.com>
2017-08-14 14:38:01 +0700

    Define named route

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,10 @@ Route::group(['prefix' => 'admin-1'], function () {
                 return view('admin-1.features.ui.icons');
             })->name('admin-1.ui-icons');
 
+            Route::get('social-icons', function () {
+                return view('admin-1.features.ui.social-icons');
+            })->name('admin-1.ui-social-icons');
+
         });
 
     });
