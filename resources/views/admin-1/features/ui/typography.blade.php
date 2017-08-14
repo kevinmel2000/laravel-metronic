45a72cfd0ac3fd6a25434217b126464b079962de
R. Kukuh <rkukuh@gmail.com>
2017-08-14 15:40:33 +0700

    Add page title

--- resources/views/admin-1/features/ui/typography.blade.php
+++ resources/views/admin-1/features/ui/typography.blade.php
@@ -15,4 +15,7 @@
 @endsection
 
 @section('content')
+    <h1 class="page-title"> Typography
+        <small>metronic and bootstrap typography for texts, headings, lists, panels and more</small>
+    </h1>
 @endsection
